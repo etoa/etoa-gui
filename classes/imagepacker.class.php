21098b21fcd1995c9536196ef63137c061dcabf5
MrCage <mrcage@etoa.ch>
2007-11-14 19:39:47 +0000

    git-svn-id: https://devel.etoa.net/svn/etoa-gui/trunk@50 9ddb851b-07ab-4ff2-becd-2ceea2d3057d

--- classes/imagepacker.class.php
+++ classes/imagepacker.class.php
@@ -10,7 +10,25 @@ class ImagePacker
 	
 	function check()
 	{
+		$check=true;
+		$rdir = getcwd();
 		
+    chdir($this->src);
+    $d = opendir(".");
+    while ($f = readdir($d))
+    {
+      if (is_dir($f) && file_exists($f."/imagepack.xml"))
+      {
+      	$f1 = $rdir."/".$this->trg."/".$f.".tar.gz";
+      	$f2 = $rdir."/".$this->trg."/".$f.".zip";
+				if (!file_exists($f1))
+					$check=false;
+				if (!file_exists($f2))
+					$check=false;
+      }
+    }
+    closedir($d);			
+    return $check;
 	}
 	
 	function pack()
@@ -19,25 +37,23 @@ class ImagePacker
 		
     chdir($this->src);
     $d = opendir(".");
-    // Remove svn data
     //exec('find -name ".svn" -type d -print | xargs rm -rf {}');
-    // Go through every directory and zip the data
-    $files = array();
-    $count = 0;
+    ob_start();
     while ($f = readdir($d))
     {
       if (is_dir($f) && file_exists($f."/imagepack.xml"))
       {
       	$f1 = $rdir."/".$this->trg."/".$f.".tar.gz";
       	$f2 = $rdir."/".$this->trg."/".$f.".zip";
-
-	passthru("tar czvf ".$f1." ".$f."/");
+				passthru("tar czvf ".$f1." ".$f."/");
         passthru("zip -r ".$f2." ".$f."/");
-	echo "Packed $f1 and $f2 <br>";
       }
     }
+    $out = ob_get_contents()
+  	ob_end_clean();  
     closedir($d);		
-}
+    return $out;
+	}
 }
 
 ?>
