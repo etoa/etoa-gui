03faf40b28641f88d543dc14dbd4495616a86c4a
MrCage <mrcage@etoa.ch>
2007-11-14 21:38:23 +0000

    Changed link in external rss list
    git-svn-id: https://devel.etoa.net/svn/etoa-gui/trunk@62 9ddb851b-07ab-4ff2-becd-2ceea2d3057d

--- index/feeds.php
+++ index/feeds.php
@@ -4,7 +4,7 @@
 	
 	echo "<div style=\"margin:0px auto;width:600px;\">";
 	echo 'Was ist ein RSS-Feed? Klicke <a href="http://de.wikipedia.org/wiki/Rss">hier</a> für mehr Infos!<br/><br/>';
-	echo '<a href="rss/townhall.rss.php">Rathaus - Aktuelle Beiträge aus dem Rathaus</a><br/>';
+	echo '<a href="cache/rss/townhall.rss">Rathaus - Aktuelle Beiträge aus dem Rathaus</a><br/>';
 
 	echo '</div><br/><br/>';
 
