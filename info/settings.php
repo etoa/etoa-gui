d14013e677bf7143f034e91e31711def59130eed
MrCage <mrcage@etoa.ch>
2007-11-22 11:13:35 +0000

    Added basic settings and resource texts in help
    git-svn-id: https://devel.etoa.net/svn/etoa-gui/trunk@100 9ddb851b-07ab-4ff2-becd-2ceea2d3057d

--- info/settings.php
+++ info/settings.php
@@ -12,9 +12,30 @@
 	$item['user_inactive_days']['p1']="Löschung wegen Inaktivität nach (in Tagen)";
 	$item['user_timeout']['v']="Timeout in Sekunden";
 
+	$item['global_time']['v']="Globaler Bauzeitfaktor";
+	$item['flight_start_time']['v']="Startzeitfaktor";
+	$item['flight_land_time']['v']="Landezeitfaktor";
+	$item['flight_flight_time']['v']="Flugzeitfaktor";
+	$item['def_build_time']['v']="Verteidigungsbauzeitfaktor";
+	$item['build_build_time']['v']="Gebäudebauzeitfaktor";
+	$item['res_build_time']['v']="Forschungszeitfaktor";
+	$item['ship_build_time']['v']="Schiffbauzeitfaktor";
+	$item['planet_temp']['p1']="Minimale Planetentemperatur";
+	$item['planet_temp']['p2']="Maximale Planetentemperatur";
+	$item['planet_fields']['p1']="Minimale Feldanzahl";
+	$item['planet_fields']['p2']="Maximale Feldanzahl";
+	$item['num_planets']['p1']="Minimale Planetenanzahl";
+	$item['num_planets']['p2']="Maximale Planetenanzahl";
+	$item['user_max_planets']['v']="Max Planeten/User";
 
-//	$item['enable_register']['p2']="Spieler";
-
+	$item['def_restore_percent']['v']="Verteidigungswiederherstellung";
+	$item['def_wf_percent']['v']="Verteidigung ins Trümmerfeld";
+	$item['ship_wf_percent']['v']="Schiffe ins Trümmerfeld";
+	$item['user_attack_min_points']['v']="Noobschutz: Min Punkte";
+	$item['user_attack_percentage']['v']="Noobschutz: Verhältnis %";
+		
+	$item['people_food_require']['v']="Nahrungsverbrauch pro Arbeiter";
+	$item['people_multiply']['v']="Bevölkerungswachstum";
 
 		infobox_start("Grundeinstellungen",1);
 		echo "<tr><td class=\"tbltitle\">Name</td>";
