97bbf0582c505b14a5b4d9e828ebcdcf4ced08eb
Yanneck <yanneck@etoa.ch>
2008-01-08 20:28:42 +0000

    Fenster des Admin-Notepads angepasst
    git-svn-id: https://devel.etoa.net/svn/etoa-gui/trunk@233 9ddb851b-07ab-4ff2-becd-2ceea2d3057d

--- admin/index.php
+++ admin/index.php
@@ -235,7 +235,7 @@
 								//
 								//Notepad
 								//
-								echo "<br><input type=\"submit\" value=\"NotePad\" name=\"NotePad\" onclick=window.open(\"misc/notepad.php?id=".$s['user_id']."\",\"notepad\")><br>";
+								echo "<br><input type=\"submit\" value=\"NotePad\" name=\"NotePad\" onclick=\"window.open('misc/notepad.php?id=".$s['user_id']."','Notepad','width=600, height=500, status=no, scrollbars=yes')\"><br>";
 
 								//
 								// Auslastung
