0dcb1f3ebf20e29009a53960225721221f670269
MrCage <mrcage@etoa.ch>
2008-02-26 19:53:43 +0000

    - Nicht mehr benoetigtes Skript geloescht
    git-svn-id: https://devel.etoa.net/svn/etoa-gui/trunk@313 9ddb851b-07ab-4ff2-becd-2ceea2d3057d

--- scripts/reset_admin_pw.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-htpasswd2 -bc cache/security/.htpasswd etoa ""
-chown etoa:apache cache/security/.htpasswd
-chmod g+w cache/security/.htpasswd
-echo "Das Admin-User wurde auf 'etoa' und das Passwort auf '' gesetzt!"
