41fb400d67653133c6e0f05012d156a278c44172
MrCage <mrcage@etoa.ch>
2008-02-26 16:49:41 +0000

    - Default db server config angepasst
    git-svn-id: https://devel.etoa.net/svn/etoa-gui/trunk@311 9ddb851b-07ab-4ff2-becd-2ceea2d3057d

--- example.conf.inc.php
+++ example.conf.inc.php
@@ -2,13 +2,13 @@
 	// EtoA main config file
 	// Rename it to conf.inc.php to get it working
 
-	define('DB_SERVER','test.etoa.net');
+	define('DB_SERVER','test.etoa.ch');
 	define('DB_USER','etoatest');
 	define('DB_PASSWORD','etoatest');
 	define('DB_DATABASE','etoatest');	
 	
 	define("PASSWORD_SALT","wokife63wigire64reyodi69");
-	define('LOGINSERVER_URL',"http://www.etoa.ch/servers");
+	define('LOGINSERVER_URL',"http://dev.etoa.ch");
 	define('ROUNDID',"Testrunde");
 	define('ETOA_DEBUG',0);
 ?>
