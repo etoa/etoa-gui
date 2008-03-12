f5c0f0ef6d9b14f9207b1266eee8067a73b2bd5b
MrCage <mrcage@etoa.ch>
2008-03-12 20:57:31 +0000

    - Werbung in eigenem File definiert
    git-svn-id: https://devel.etoa.net/svn/etoa-gui/trunk@349 9ddb851b-07ab-4ff2-becd-2ceea2d3057d

---
+++ inc/adds.inc.php
@@ -0,0 +1,44 @@
+<?PHP
+	// Werbebanner
+	define('ADD_BANNER','
+	<div style="color:#0f0;font-size:8pt;font-weight:bold;">Unterstütze EtoA:<br/>
+	(SHIFT+Click = neues Fenster)</div><br/>
+	<script type="text/javascript"><!--
+google_ad_client = "pub-4873671285923921";
+google_ad_width = 120;
+google_ad_height = 600;
+google_ad_format = "120x600_as";
+google_ad_type = "text";
+//2007-08-16: EtoA InGame
+google_ad_channel = "8017728947";
+google_color_border = "000000";
+google_color_bg = "000000";
+google_color_link = "FFFFFF";
+google_color_text = "CCCCCC";
+google_color_url = "999999";
+//-->
+</script>
+<script type="text/javascript"
+  src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
+</script><br/>
+	<script type="text/javascript"><!--
+google_ad_client = "pub-4873671285923921";
+google_ad_width = 120;
+google_ad_height = 600;
+google_ad_format = "120x600_as";
+google_ad_type = "text";
+//2007-08-16: EtoA InGame
+google_ad_channel = "8017728947";
+google_color_border = "000000";
+google_color_bg = "000000";
+google_color_link = "FFFFFF";
+google_color_text = "CCCCCC";
+google_color_url = "999999";
+//-->
+</script>
+<script type="text/javascript"
+  src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
+</script>
+
+');
+?>
