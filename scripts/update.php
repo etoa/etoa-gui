d990f2f1b77dbd6ba52b71bdba592d39495b4ee6
MrCage <mrcage@etoa.ch>
2008-07-25 12:15:57 +0000

    - Temporarily disabled wormhole update, it regularly produces cron errors
    git-svn-id: https://devel.etoa.net/svn/etoa-gui/trunk@676 9ddb851b-07ab-4ff2-becd-2ceea2d3057d

--- scripts/update.php
+++ scripts/update.php
@@ -77,9 +77,9 @@
 		$log = "\nPunkte aktualisiert.\nDauer: ".timerStop($tmr)." sec\n\n";
 
 		// Wurmlöcher vertauschen
-		$tmr = timerStart();
-		Wormhole::randomize();
-		$log.= "Wurml&ouml;cher vertauscht.\nDauer: ".timerStop($tmr)." sec\n\n";
+		//$tmr = timerStart();
+		//Wormhole::randomize();
+		//$log.= "Wurml&ouml;cher vertauscht.\nDauer: ".timerStop($tmr)." sec\n\n";
 
 		// Closes all open tables, forces all tables in use to be closed, and flushes the query cache.
 		dbquery("FLUSH TABLES");
