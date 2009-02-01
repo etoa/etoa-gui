84d763aeec2bfc2dd3e11879290ada1d891f4999
Glaubinix <glaubinix@etoa.ch>
2009-02-01 13:30:20 +0000

    Fixed Exp for non special planet ships

--- src/objects/ListShip.h
+++ src/objects/ListShip.h
@@ -24,23 +24,21 @@ public:
 		this->initCount = this->count;
 		
 		this->special = (bool)oRow["shiplist_special_ship"];
-		if (this->special) {
-			this->sLevel = (short)oRow["shiplist_special_ship_level"];
-			this->sExp = (int)oRow["shiplist_special_ship_exp"];
-			this->sBonusWeapon = (short)oRow["shiplist_special_ship_bonus_weapon"];
-			this->sBonusStructure = (short)oRow["shiplist_special_ship_bonus_structure"];
-			this->sBonusShield = (short)oRow["shiplist_special_ship_bonus_shield"];
-			this->sBonusHeal = (short)oRow["shiplist_special_ship_bonus_heal"];
-			this->sBonusCapacity = (short)oRow["shiplist_special_ship_bonus_capacity"];
-			this->sBonusSpeed = (short)oRow["shiplist_special_ship_bonus_speed"];
-			this->sBonusPilots = (short)oRow["shiplist_special_ship_bonus_pilots"];
-			this->sBonusTarn = (short)oRow["shiplist_special_ship_bonus_tarn"];
-			this->sBonusAntrax = (short)oRow["shiplist_special_ship_bonus_antrax"];
-			this->sBonusForsteal = (short)oRow["shiplist_special_ship_bonus_forsteal"];
-			this->sBonusBuildDestroy = (short)oRow["shiplist_special_ship_bonus_build_destroy"];
-			this->sBonusAntraxFood = (short)oRow["shiplist_special_ship_bonus_antrax_food"];
-			this->sBonusDeactivade = (short)oRow["shiplist_special_ship_bonus_deactivade"];
-		}
+		this->sLevel = (short)oRow["shiplist_special_ship_level"];
+		this->sExp = (int)oRow["shiplist_special_ship_exp"];
+		this->sBonusWeapon = (short)oRow["shiplist_special_ship_bonus_weapon"];
+		this->sBonusStructure = (short)oRow["shiplist_special_ship_bonus_structure"];
+		this->sBonusShield = (short)oRow["shiplist_special_ship_bonus_shield"];
+		this->sBonusHeal = (short)oRow["shiplist_special_ship_bonus_heal"];
+		this->sBonusCapacity = (short)oRow["shiplist_special_ship_bonus_capacity"];
+		this->sBonusSpeed = (short)oRow["shiplist_special_ship_bonus_speed"];
+		this->sBonusPilots = (short)oRow["shiplist_special_ship_bonus_pilots"];
+		this->sBonusTarn = (short)oRow["shiplist_special_ship_bonus_tarn"];
+		this->sBonusAntrax = (short)oRow["shiplist_special_ship_bonus_antrax"];
+		this->sBonusForsteal = (short)oRow["shiplist_special_ship_bonus_forsteal"];
+		this->sBonusBuildDestroy = (short)oRow["shiplist_special_ship_bonus_build_destroy"];
+		this->sBonusAntraxFood = (short)oRow["shiplist_special_ship_bonus_antrax_food"];
+		this->sBonusDeactivade = (short)oRow["shiplist_special_ship_bonus_deactivade"];
 	}
 	
 	~ListShip() {
