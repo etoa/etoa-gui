ddde2017f12eb84e279f750736f62b2ae127a46c
Glaubinix <glaubinix@etoa.ch>
2009-03-16 10:08:39 +0000

    Added fleetcontroll mask discovery settings with config value

--- src/building/BuildingHandler.cpp
+++ src/building/BuildingHandler.cpp
@@ -6,9 +6,98 @@ namespace building
 	void BuildingHandler::update()
 	{
 		std::time_t time = std::time(0);
+		Config &config = Config::instance();
 		
-		// Load planets who needs updating
+		// Load fleetcontrolls to update discoverymask
 		mysqlpp::Query query = con_->query();
+		query << "SELECT "
+		<< "	buildlist_user_id, "
+		<< "	buildlist_current_level, "
+		<< "	cells.sx, "
+		<< "	cells.sy, "
+		<< "	cells.cx, "
+		<< "	cells.cy "
+		<< "FROM "
+		<< "	buildlist "
+		<< "	INNER JOIN "
+		<< "		entities "
+		<< "	ON "
+		<< "		entities.id=buildlist_entity_id "
+		<< "		INNER JOIN "
+		<< "			cells "
+		<< "		ON "
+		<< "			cells.id=entities.cell_id "
+		<< "WHERE "
+		<< "	buildlist_build_type>2 "
+		<< "	AND buildlist_building_id='" << config.idget("FLEET_CONTROL_ID") << "' "
+		<< "	AND buildlist_build_end_time<" << time << " ORDER BY buildlist_entity_id;";
+		mysqlpp::Result res = query.store();		
+		query.reset();
+		
+		if (res) {
+			unsigned int resSize = res.size();
+			if (resSize>0) {
+				
+				int sxNum = config.nget("num_of_sectors",1);
+				int cxNum = config.nget("num_of_cells",1);
+				int syNum = config.nget("num_of_sectors",2);
+				int cyNum = config.nget("num_of_cells",2);
+				
+				// the mask
+				char mask[10000] = "";
+				
+				mysqlpp::Row row;
+				
+				for (mysqlpp::Row::size_type i = 0; i<resSize; i++) {
+	    			row = res.at(i);
+					
+					query << "SELECT "
+					<< "	discoverymask "
+					<< "FROM "
+					<< "	users "
+					<< "WHERE "
+					<< "	user_id='" << (int)row["buildlist_user_id"] << "' "
+					<< "LIMIT 1;";
+					mysqlpp::Result maskRes = query.store();
+					query.reset();
+					
+					if (maskRes) {
+						int maskSize = maskRes.size();
+						
+						if (maskSize > 0) {
+							mysqlpp::Row maskRow = maskRes.at(0);
+							strcpy( mask, maskRow["discoverymask"]);
+						}
+					}
+					
+					int radius = 1 + (int)row["buildlist_current_level"] * config.nget("discoverymask",0);
+					int absX = (10 * ((int)row["sx"] - 1) + (int)row["cx"]);
+					int absY = (10 * ((int)row["sy"] - 1) + (int)row["cy"]);
+					
+					for (int x = absX - radius; x <= absX + radius; x++) {
+						for (int y = absY - radius; y <= absY + radius; y++) {
+							int pos = x + (cyNum * syNum) * (y - 1) - 1;
+							if (pos >= 0 && pos <= sxNum * syNum * cxNum * cyNum) {
+								mask[pos] = '1';
+							}
+						}
+					}
+					
+					// Update the mask
+					query << "UPDATE "
+					<< "	users "
+					<< "SET "
+					<< " discoverymask='" << mask << "' "
+					<< "WHERE "
+					<< "	user_id='" << (int)row["buildlist_user_id"] << "' "
+					<< "LIMIT 1;";
+					query.store();
+					query.reset();
+				}
+			}
+		}
+				
+		// Load planets who needs updating
 		query << "SELECT "
 			<< "	buildlist_entity_id "
 			<< "FROM "
@@ -16,7 +105,7 @@ namespace building
 			<< "WHERE "
 			<< "	buildlist_build_type>2 "
 			<< "	AND buildlist_build_end_time<" << time << " ORDER BY buildlist_entity_id;";
-		mysqlpp::Result res = query.store();		
+		res = query.store();		
 		query.reset();
 		
 		// Add changed planets to vector
