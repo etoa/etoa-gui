4c2a3b4b15895477fd4ac87e4ab3d06032ca1bd1
MrCage <mrcage@etoa.ch>
2009-05-28 09:07:23 +0000

    - Generalization in report view
    git-svn-id: https://devel.etoa.net/svn/etoa-gui/trunk@1055 9ddb851b-07ab-4ff2-becd-2ceea2d3057d

--- content/reports.php
+++ content/reports.php
@@ -1,4 +1,24 @@
 <?php
+	//////////////////////////////////////////////////
+	//		 	 ____    __           ______       			//
+	//			/\  _`\ /\ \__       /\  _  \      			//
+	//			\ \ \L\_\ \ ,_\   ___\ \ \L\ \     			//
+	//			 \ \  _\L\ \ \/  / __`\ \  __ \    			//
+	//			  \ \ \L\ \ \ \_/\ \L\ \ \ \/\ \   			//
+	//	  		 \ \____/\ \__\ \____/\ \_\ \_\  			//
+	//			    \/___/  \/__/\/___/  \/_/\/_/  	 		//
+	//																					 		//
+	//////////////////////////////////////////////////
+	// The Andromeda-Project-Browsergame				 		//
+	// Ein Massive-Multiplayer-Online-Spiel			 		//
+	// Programmiert von Nicolas Perrenoud				 		//
+	// www.nicu.ch | mail@nicu.ch								 		//
+	// als Maturaarbeit '04 am Gymnasium Oberaargau	//
+	//////////////////////////////////////////////////
+	//
+	// 	$Id$
+	//
+
 	echo "<h1>Berichte</h1>";
 
 /*
@@ -20,56 +40,22 @@
 
 	$type = isset($_GET['type']) ? $_GET['type'] : 'all';
 
-	if ($type!="all")
-	{
+	if ($type == "all")
+		$reports = Report::find(array("user_id"=>$cu->id),"timestamp DESC");
+	else
 		$reports = Report::find(array("type"=>$type,"user_id"=>$cu->id),"timestamp DESC");
 
-		if (count($reports)>0)
-		{
-			tableStart("Marktberichte");
-			echo "<tr>
-			<th colspan=\"2\">Nachricht:</th>
-			<th>Datum:</th>
-			</tr>";
-
-			foreach ($reports as $rid => $r)
-			{
-				if (!$r->read)
-				{
-					$im_path = "images/pm_new.gif";
-				}
-				else
-				{
-					$im_path = "images/pm_normal.gif";
-				}
-				echo "<tr>";
-				echo "<td style=\"width:16px\"><img src=\"".$im_path."\" alt=\"Mail\" id=\"repimg".$rid."\" /></td>";
-				echo "<td>";
-				echo "<a href=\"javascript:;\" onclick=\"toggleBox('report".$rid."');xajax_reportSetRead(".$rid.")\" >".$r->subject."</a>";
-				echo "</td>
-				<td>".df($r->timestamp)."</td>
-				</tr>";
-				echo "<tr><td colspan=\"3\" style=\"padding:10px;display:none;\" id=\"report".$rid."\">";
-				echo $r;
-				echo "</td>";
-				echo "</tr>";
-			}
-			tableEnd();
-		}
-		else
-		{
-			err_msg("Keine Berichte vorhanden!");
-		}
-	}
-	else
+	if (count($reports)>0)
 	{
-		$reports = Report::find(array("user_id"=>$cu->id),"timestamp DESC");
-
-		tableStart("Neueste Berichte");
+		if ($type == "all")
+			tableStart("Neueste Berichte");
+		else
+			tableStart(Report::$types[$type]."berichte");
 		echo "<tr>
-		<th colspan=\"2\">Nachricht:</th>
-		<th style=\"width:100px;\">Kategorie:</th>
-		<th style=\"width:150px\">Datum:</th>
+		<th colspan=\"2\">Nachricht:</th>";
+		if ($type == "all")
+			echo "<th style=\"width:100px;\">Kategorie:</th>";
+		echo "<th style=\"width:150px\">Datum:</th>
 		</tr>";
 
 		foreach ($reports as $rid => $r)
@@ -84,14 +70,19 @@
 			}
 			echo "<tr>
 			<td style=\"width:16px\"><img src=\"".$im_path."\" alt=\"Mail\" id=\"repimg".$rid."\" /></td>
-			<td><a href=\"javascript:;\" onclick=\"toggleBox('report".$rid."');xajax_reportSetRead(".$rid.")\" >".$r->subject."</a></td>
-			<td><b>".$r->typeName()."</b></td>
-			<td>".df($r->timestamp)."</td></tr>";
+			<td><a href=\"javascript:;\" onclick=\"toggleBox('report".$rid."');xajax_reportSetRead(".$rid.")\" >".$r->subject."</a></td>";
+			if ($type == "all")
+				echo "<td><b>".$r->typeName()."</b></td>";
+			echo "<td>".df($r->timestamp)."</td></tr>";
 			echo "<tr><td colspan=\"4\" style=\"padding:10px;display:none;\" id=\"report".$rid."\">";
 			echo $r;
 			echo "</td>";
 			echo "</tr>";
 		}
 		tableEnd();
+	}
+	else
+	{
+		err_msg("Keine Berichte vorhanden!");
 	}
 ?>
