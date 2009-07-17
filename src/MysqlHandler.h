9504395760318537278b67477b51b8f4f01c1c55
Glaubinix <glaubinix@etoa.ch>
2009-07-17 15:17:53 +0000

    .

--- src/MysqlHandler.h
+++ src/MysqlHandler.h
@@ -44,7 +44,7 @@
 			mysqlpp::Connection con((mysql["DB_DATABASE"]).c_str(),(mysql["DB_SERVER"]).c_str(),(mysql["DB_USER"]).c_str(),(mysql["DB_PASSWORD"]).c_str());
 			
 			if (!con) {
-				cerr << "Database connection failed: " << con.error() << endl;
+				std::cerr << "Database connection failed: " << con.error() << std::endl;
 				exit(EXIT_FAILURE);
 			}
 			con_ = con;
