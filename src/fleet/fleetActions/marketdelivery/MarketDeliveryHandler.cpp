0a006e3e8135e1b4f140e03e01d8aed07bbcae0e
Glaubinix <glaubinix@etoa.ch>
2009-07-17 16:19:01 +0000

    .

--- src/fleet/fleetActions/marketdelivery/MarketDeliveryHandler.cpp
+++ src/fleet/fleetActions/marketdelivery/MarketDeliveryHandler.cpp
@@ -28,6 +28,7 @@ namespace marketdelivery
 		marketMesage->addText("[b]Zeit:[/b] ");
 		marketMesage->addText(this->f->getLandtimeString(),1);
 		marketMesage->addText("[b]Bericht:[/b] Die gekauften Waren sind angekommen.",1);
+		marketMesage->addText(this->msgShips,1);
 		marketMesage->addText(this->msgRes,1);
 		
 		marketMesage->addSignature("Unser Unternehmen dankt ihnen f&uuml;r die Unterst&uuml;tzung und wir hoffen sie sind mit uns zufrieden und w&uuml;nschen ihnen auch in Zukunft viel Erfolg.\nDas Handelsministerium");
