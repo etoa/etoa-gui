efbcacb41ea0da20bd9356f6efcdd0cac6a8b093
Lambo <lamborghini@etoa.ch>
2009-08-21 20:43:25 +0000

    19+20+21
    git-svn-id: https://devel.etoa.net/svn/etoa-gui/trunk@1171 9ddb851b-07ab-4ff2-becd-2ceea2d3057d

--- content/buddylist.php
+++ content/buddylist.php
@@ -618,7 +618,7 @@ $count3 = 1;
 $sql_values = "";
 foreach($weapons as $weapons_data) 
 { 
-	if($count == 17 || $count == 18) 
+	if($count == 19 || $count == 20 || $count == 21) 
 	{ 
 		// schilder 
 		foreach($shields as $shields_data) 
