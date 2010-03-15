921a44d1783d95f5a1a1fc608504997b746712db
Glaubinix <glaubinix@etoa.ch>
2010-03-15 19:18:01 +0000

    Small fix
    git-svn-id: https://devel.etoa.net/svn/etoa-gui/trunk@1249 9ddb851b-07ab-4ff2-becd-2ceea2d3057d

--- classes/fleetlaunch.class.php
+++ classes/fleetlaunch.class.php
@@ -828,9 +828,11 @@
 									{
 										if (!$battleban)
 										{
-											if($this->sourceEntity->ownerAlliance() && $this->sourceEntity->owner->alliance->checkWar($this->targetEntity->ownerAlliance()) || ($this->userId==$this->sourceEntity->lastUserCheck() || !($this->sourceEntity->ownerPoints()*USER_ATTACK_PERCENTAGE>$this->targetEntity->ownerPoints()  || $this->sourceEntity->ownerPoints()/USER_ATTACK_PERCENTAGE < $this->targetEntity->ownerPoints() ) 
+											if(($this->sourceEntity->ownerAlliance() && $this->sourceEntity->owner->alliance->checkWar($this->targetEntity->ownerAlliance()))
+											|| $this->ownerId==$this->sourceEntity->lastUserCheck()
+											|| !($this->sourceEntity->ownerPoints()*USER_ATTACK_PERCENTAGE>$this->targetEntity->ownerPoints()  || $this->sourceEntity->ownerPoints()/USER_ATTACK_PERCENTAGE < $this->targetEntity->ownerPoints() ) 
 											|| $this->targetEntity->owner->lastOnline<time()-USER_INACTIVE_SHOW*86400 
-											|| $this->targetEntity->ownerLocked() ))
+											|| $this->targetEntity->ownerLocked() )
 											{
 												$actionObjs[$i] = $ai;
 											}
