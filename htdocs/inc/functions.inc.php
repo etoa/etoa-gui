30e5e6ce02f94d8b4e8e2cc6af91a64659f1513b
MrCage <mrcage@etoa.ch>
2012-04-13 17:35:37 +0000

    - fixed ipc message queue token
    git-svn-id: https://devel.etoa.net/svn/etoa-gui/trunk@1451 9ddb851b-07ab-4ff2-becd-2ceea2d3057d

--- htdocs/inc/functions.inc.php
+++ htdocs/inc/functions.inc.php
@@ -2697,7 +2697,7 @@ function imagecreatefromfile($path, $user_functions = false)
 	{
 		if (function_exists("msg_get_queue"))
 		{
-			$dname = dirname(realpath("config/db.config.php"));
+			$dname = realpath("config/");
 			$ipckey = ftok($dname,IPC_ID);
 			$q = msg_get_queue($ipckey,0666);
 			add_log(4,"Sende IPC Message mit Key $ipckey vom Token $dname und Projekt-Id ".IPC_ID.". Die Queue hat die ID ".$q);
