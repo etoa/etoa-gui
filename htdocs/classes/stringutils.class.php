8c8fb4df62ae87d829045fe8dd9040daf5d31115
MrCage <mrcage@etoa.ch>
2013-02-16 14:49:58 +0000

    Added StringUtils class with splitBySpaces method
    git-svn-id: https://devel.etoa.net/svn/etoa-gui/trunk@1555 9ddb851b-07ab-4ff2-becd-2ceea2d3057d

---
+++ htdocs/classes/stringutils.class.php
@@ -0,0 +1,30 @@
+<?PHP
+
+	/**
+	* String utilities
+  *
+  * @author MrCage <mrcage@etoa.ch>
+	*/
+	class StringUtils
+	{
+    /**
+    * Splits a string of words into an array and treats words in quotes as a single word
+    *
+    * @see http://stackoverflow.com/questions/2202435/php-explode-the-string-but-treat-words-in-quotes-as-a-single-word
+    */
+    static function splitBySpaces($text) {
+      if (preg_match_all('/"(?:\\\\.|[^\\\\"])*"|\S+/', $text, $matches)) {
+        $rtn = array();
+        for ($x=0; $x < count($matches[0]); $x++) {
+          $rtn[$x] = preg_replace(array('/^"/', '/"$/', '/\\\"/'), array('', '', '"'), $matches[0][$x]);
+        }
+        return $rtn;
+      } else {
+        if (strlen($text) > 0) {
+          return array($text);
+        }
+        return array();
+      }
+    }    
+	}
+?>
