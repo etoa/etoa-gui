6fcec61cf8a522f1179b37e8ac2e99729e322e7a
Nicolas Perrenoud (mrcage) <mrcage@etoa.ch>
2013-04-17 20:29:01 +0000

    Moved code from header into cpp file

--- src/planet/PlanetEntity.cpp
+++ src/planet/PlanetEntity.cpp
@@ -3,6 +3,128 @@
 
 namespace planet
 {
+	PlanetEntity::PlanetEntity(int entityId) {
+
+		this->smallUpdate = false;
+		
+		Config &config = Config::instance();
+					
+		this->entityId = entityId;
+			
+		this->store.resize(6);
+		this->cnt.resize(8);
+		this->ressource.resize(7);
+		this->bunker.resize(5);
+			
+		this->loadData();
+		
+		DataHandler &DataHandler = DataHandler::instance();
+			
+		this->race_ = DataHandler.getRaceById(this->raceId);
+		this->sol_ = DataHandler.getSolById(this->solType);
+		this->planet_ = DataHandler.getPlanetById(this->planetType);
+		this->specialist_ = DataHandler.getSpecialistById(this->speicalistId);
+			
+		this->updateValues();
+			
+		this->fieldsUsed = 0;
+		this->fieldsExtra = 0;
+			
+		this->bunkerRes = 0;
+			
+		this->store[0] = config.nget("def_store_capacity", 0);
+		this->store[1] = config.nget("def_store_capacity", 0);
+		this->store[2] = config.nget("def_store_capacity", 0);
+		this->store[3] = config.nget("def_store_capacity", 0);
+		this->store[4] = config.nget("def_store_capacity", 0);
+		this->store[5] = config.nget("user_start_people", 1);
+			
+		this->cnt[0] = 0;
+		this->cnt[1] = 0;
+		this->cnt[2] = 0;
+		this->cnt[3] = 0;
+		this->cnt[4] = 0;
+		this->cnt[5] = 0;
+		this->cnt[6] = 0;
+		this->cnt[7] = 0;
+			
+		this->loadBuildlist();
+		this->loadShiplist();
+		this->loadDeflist();
+		this->addBoni();
+	}
+
+	PlanetEntity::PlanetEntity(mysqlpp::Row& planet) {
+
+		this->entityId = (int)planet["id"];
+			
+		this->t = time(0) - (int)planet["planet_last_updated"];
+			
+		this->smallUpdate = true;
+			
+		this->store.resize(6);
+		this->cnt.resize(8);
+		this->ressource.resize(7);
+		this->bunker.resize(5);
+			
+		this->ressource[0] = (double)planet["planet_res_metal"];
+		this->ressource[1] = (double)planet["planet_res_crystal"];
+		this->ressource[2] = (double)planet["planet_res_plastic"];
+		this->ressource[3] = (double)planet["planet_res_fuel"];
+		this->ressource[4] = (double)planet["planet_res_food"];
+		this->ressource[5] = (double)planet["planet_people"];
+		this->ressource[6] = 0;
+		
+		this->store[0] = (double)planet["planet_store_metal"];
+		this->store[1] = (double)planet["planet_store_crystal"];
+		this->store[2] = (double)planet["planet_store_plastic"];
+		this->store[3] = (double)planet["planet_store_fuel"];
+		this->store[4] = (double)planet["planet_store_food"];
+		this->store[5] = (double)planet["planet_people_place"];
+			
+		this->cnt[0] = (double)planet["planet_prod_metal"];
+		this->cnt[1] = (double)planet["planet_prod_crystal"];
+		this->cnt[2] = (double)planet["planet_prod_plastic"];
+		this->cnt[3] = (double)planet["planet_prod_fuel"];
+		this->cnt[4] = (double)planet["planet_prod_food"];
+		this->cnt[5] = 0;
+		this->cnt[6] = 0;
+		this->cnt[7] = 0;
+			
+		this->bunker[0] = (double)planet["planet_bunker_metal"];
+		this->bunker[1] = (double)planet["planet_bunker_crystal"];
+		this->bunker[2] = (double)planet["planet_bunker_plastic"];
+		this->bunker[3] = (double)planet["planet_bunker_fuel"];
+		this->bunker[4] = (double)planet["planet_bunker_food"];
+			
+		this->raceId = (int)planet["user_race_id"];
+		this->userId = (int)planet["user_id"];
+		this->solType = (int)planet["type_id"];
+		this->planetType = (int)planet["planet_type_id"];
+			
+		this->isMain = (bool)planet["planet_user_main"];
+		this->isUmod = (int)planet["user_hmode_to"] > 0 ? true : false;
+
+                this->speicalistId = (int)planet["user_specialist_time"] < time(0) ? 0 : (int)planet["user_specialist_id"];
+
+                DataHandler &DataHandler = DataHandler::instance();
+
+                this->race_ = DataHandler.getRaceById(this->raceId);
+                this->sol_ = DataHandler.getSolById(this->solType);
+                this->planet_ = DataHandler.getPlanetById(this->planetType);
+                this->specialist_ = DataHandler.getSpecialistById(this->speicalistId);
+
+                this->updateValues();
+        }
+
+	PlanetEntity::~PlanetEntity() {
+		if (smallUpdate)
+			this->saveRes();
+		else
+			this->save();
+	
+	}
+
 	void PlanetEntity::loadData() {
 		My &my = My::instance();
 		mysqlpp::Connection* con_ = my.get();
