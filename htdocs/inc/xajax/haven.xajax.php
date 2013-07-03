97ed0fb52fe612db09d100aa515956ef1d068465
River <river@etoa.ch>
2013-07-03 22:04:53 +0000

    Added more verbosity to double-wormhole action log
    git-svn-id: https://devel.etoa.net/svn/etoa-gui/trunk@1638 9ddb851b-07ab-4ff2-becd-2ceea2d3057d

--- htdocs/inc/xajax/haven.xajax.php
+++ htdocs/inc/xajax/haven.xajax.php
@@ -952,7 +952,10 @@
 			else
 			{
 				include_once(getcwd().'/inc/bootstrap.inc.php');
-				add_log(18,'Der User '.$_SESSION['user_nick'].' versuchte, ein zweites Wurmloch zu &ouml;ffnen');
+				add_log(18,'Der User '.$_SESSION['user_nick'].' versuchte, ein zweites Wurmloch zu &ouml;ffnen <br />'
+						.'Bereits gesetztes Wurmloch: '.$fleet->wormholeEntryEntity.' mit Austrittspunkt '.$fleet->wormholeExitEntity .'<br />'
+						.'Zweites Wumloch: '.$form['man_sx'].' / '.$form['man_sy'].' : '.$form['man_cx'].' / '.$form['man_cy'].' : '.$form['man_p'].'.'
+				);
 				$response->alert("Wurmloch wurde bereits gesetzt!");
 			}
 
