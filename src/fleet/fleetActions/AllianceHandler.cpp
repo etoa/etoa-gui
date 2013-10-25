a33baba8916dbafa206459e28e3a966296f9bd62
river <river@etoa.ch>
2013-10-25 17:15:32 +0200

    reformatted source of AllianceHandler.cpp

--- src/fleet/fleetActions/AllianceHandler.cpp
+++ src/fleet/fleetActions/AllianceHandler.cpp
@@ -3,34 +3,34 @@
 
 namespace alliance
 {
-	void AllianceHandler::update()
-	{
-		/**
-		* Fleet-Action: Attack
-		*/
-		
-		Config &config = Config::instance();
-		
-		if(config.nget("alliance_fleets_max_players",0))
-		{
-		    f->sendHomeExceedingAllianceFleets
-		    (
-			(unsigned int) config.nget("alliance_fleets_max_players",1),
-			this->targetEntity->getUserId(),
-			this->actionLog
-		    );
-		}
-		
-		BattleHandler *bh = new BattleHandler();
-		bh->battle(this->f,this->targetEntity,this->actionLog);
-		
-		// if fleet user has won the fight, send fleet home
-		if (bh->returnFleet)
-		{
-			this->f->setReturn();
-		}
-		
-		delete bh;
-	}
+void AllianceHandler::update()
+{
+    /**
+    * Fleet-Action: Attack
+    */
+
+    Config &config = Config::instance();
+
+    if (config.nget("alliance_fleets_max_players",0))
+    {
+        f->sendHomeExceedingAllianceFleets
+        (
+            (unsigned int) config.nget("alliance_fleets_max_players",1),
+            this->targetEntity->getUserId(),
+            this->actionLog
+        );
+    }
+
+    BattleHandler *bh = new BattleHandler();
+    bh->battle(this->f,this->targetEntity,this->actionLog);
+
+    // if fleet user has won the fight, send fleet home
+    if (bh->returnFleet)
+    {
+        this->f->setReturn();
+    }
+
+    delete bh;
+}
 }
 
