45dc4caa2e875a5ddf54b17a5c74fd6af0f83211
MrCage <mrcage@etoa.ch>
2013-12-04 23:35:30 +0100

    Fixed user stats png generation (missing directory)

--- htdocs/classes/userstats.class.php
+++ htdocs/classes/userstats.class.php
@@ -166,13 +166,17 @@
 		imagestring($im, 2, 500, $h-25, $avgr, $colBlack);	
 		imagestring($im, 2, 605, $h-25, $actr, $colBlack);	
 	
-    if (is_file($file)) {
-      unlink($file);		
-    }
+		$dir = dirname($file);
+		if (!is_dir($dir)) {
+			mkdir($dir, 0777, true);
+		}
+
+		if (is_file($file)) {
+		  unlink($file);
+		}
 		imagepng($im,$file);
 
 		}
-		
 	}
 	
 	static function generateXml($file)
