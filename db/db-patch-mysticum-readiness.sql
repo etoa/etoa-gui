80987635740a655cb14382be5922dec29d2339c3
MrCage <mrcage@etoa.ch>
2013-12-05 20:42:23 +0100

    Implementent special ship readyness bonus in fleet launch process

--- db/db-patch-mysticum-readiness.sql
+++ db/db-patch-mysticum-readiness.sql
@@ -1,2 +1,3 @@
 ALTER TABLE `ships` ADD `special_ship_bonus_readiness` DECIMAL( 4, 2 ) NOT NULL AFTER `special_ship_bonus_deactivade`;
 ALTER TABLE `shiplist` ADD `shiplist_special_ship_bonus_readiness` TINYINT( 2 ) UNSIGNED NOT NULL DEFAULT '0';
+ALTER TABLE `fleet_ships` ADD `fs_special_ship_bonus_readiness` TINYINT( 2 ) UNSIGNED NOT NULL DEFAULT '0';
