0d8d3aa0a1c2d0a0f891ace4cca1ff2a54409e7d
river <river@etoa.ch>
2013-12-18 19:56:59 +0100

    Fixed #41: PHP htmlentities() is broken. Added manual replace.

--- htdocs/classes/stringutils.class.php
+++ htdocs/classes/stringutils.class.php
@@ -54,7 +54,7 @@
 	  // NOTE: if the text contains html text, it is displayed as html, not as plaintext.
 	  // if you want to display text with plaintext <br> etc., wrap the INPUT like that:
 	  // htmlentities($input_string,ENT_QUOTES,'UTF-8')
-	  return str_replace("\\","\\\\",htmlentities($str,ENT_QUOTES,'UTF-8'));
+	  return str_replace("\\","\\\\",htmlentities(str_replace("'",'&apos;',$str), ENT_QUOTES,'UTF-8'));
 	}
     
     public static function replaceAsciiControlCharsUnicode($str)
