aea1e13a92b09f1dea1f240f9f700443179dc54b
river <river@etoa.ch>
2014-05-14 23:00:39 +0200

    Fixed unreachable code in building type info

--- htdocs/info/buildings.php
+++ htdocs/info/buildings.php
@@ -642,26 +642,27 @@ elseif (isset($_GET['aid']))
 //
 // Kategorieinfos
 //
-elseif(isset($_GET['type_id']) && $_GET['type_id']>0)
+elseif(isset($_GET['type_id']) && intval($_GET['type_id'])>0)
 {
+	$btid = intval($_GET['type_id']);
 
-	if ($_GET['id']==BUILDING_STORE_CAT)
+	if ($btid==BUILDING_STORE_CAT)
 	{
 		echo "<b>Lagerkapazit&auml;t</b><br>";
 		echo "Du kannst auf einem Planeten nicht unentlich viele Rohstoffe lagern. Jeder Planet hat eine Lagerkapazit&auml;t von ".intval($conf['def_store_capacity']['v']).". Um die Lagerkapazit&auml;t zu erh&ouml;hen, kannst du eine Planetenbasis und danach verschiedene Speicher, Lagerhallen und Silos bauen, welche die Kapazit&auml;t erh&ouml;hen. Wenn eine Zahl in der Rohstoffanzeige rot gef&auml;rbt ist, bedeutet das, dass dieser Rohstoff die Lagerkapazit&auml;t &uuml;berschreitet. Baue in diesem Fall den Speicher aus. Eine &uuml;berschrittene Lagerkapazit&auml;t bedeutet, dass nichts mehr produziert wird, jedoch werden Rohstoffe, die z.B. mit einer Flotte ankommen, trotzdem auf dem Planeten gespeichert.<br>";
 	}
-	elseif($_GET['id']==BUILDING_POWER_CAT)
+	elseif($btid==BUILDING_POWER_CAT)
 	{
 		echo "<b>Energie</b><br>";
 		echo "Wo es eine Produkion hat, braucht es auch Energie. Diese Energie, welche von verschiedenen Anlagen gebraucht wird, spenden uns verschiedene Kraftwerkstypen. Je h&ouml;her diese Ausgebaut sind, desto mehr Leistung erbringen sie und versorgen so die wachsende Wirtschaft.<br>
 		Hat es zu wenig Energie, wird die Produktion prozentual gedrosselt, was verheerende Auswirkungen haben kann!";
 	}
-	elseif($_GET['id']==BUILDING_GENERAL_CAT)
+	elseif($btid==BUILDING_GENERAL_CAT)
 	{
 		echo "<b>Allgemeine Geb&auml;ude</b><br/>";
 		echo "Diese Geb&auml;ude werden ben&ouml;tigt um deinen Planeten auszubauen und die Produktion und Forschung zu erm&ouml;glichen.";
 	}
-	elseif($_GET['id']==BUILDING_RES_CAT)
+	elseif($btid==BUILDING_RES_CAT)
 	{
 		echo "<b>Rohstoffgeb&auml;ude</b><br/>";
 		echo "Diese Geb&auml;ude liefern Rohstoffe, welche du f&uuml;r den Aufbau deiner Zivilisation brauchst.";
