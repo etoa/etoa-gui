3f10a3da92b86876c67fc9c97a9a8af3f19a28f7
MrCage <mrcage@etoa.ch>
2014-10-17 08:34:56 +0200

    Added BackendCheckTask

--- htdocs/inc/update.inc.php
+++ htdocs/inc/update.inc.php
@@ -196,8 +196,6 @@
 
 	function update_5minute()
 	{
-		$cfg = Config::getInstance();
-
 		$log = '';
 		
 		// Update user statistics
@@ -217,23 +215,8 @@
 		
 		// Check Backend
 		$tmr = timerStart();
-		$backend = checkDaemonRunning($cfg->daemon_pidfile)>0 ? true : false;
-		$change = $cfg->value("backend_status") != $backend;
-		if ($change)
-		{
-			$status = $cfg->value("backend_status") == 0 ? 1 : 0;
-			$cfg->set("backend_status", $status);
-
-			$tm = new TextManager();
-			$infoText = $tm->getText('backend_offline_message');
-			$mailText = $cfg->value("backend_status") == 0 ? "Funktioniert wieder" : $infoText->content;
-			$mail = new Mail("EtoA-Backend", $mailText);
-			$sendTo = explode(";",$cfg->value("backend_offline_mail"));
-			foreach ($sendTo as $sendMail)	{
-				$mail->send($sendMail);
-			}
-		}
-		$log.= "Backend Check: ".($backend ? 'gestartet' : 'gestoppt')." (".($change ? 'geändert' : 'keine Änderung').") (".timerStop($tmr)." sec)\n";
+		$task = new BackendCheckTask();
+		$log.= $task->run()." (".timerStop($tmr)." sec)\n";
 		
 		return $log;
 	}
