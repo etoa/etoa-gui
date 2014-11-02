af380c3431c37ea5f4c94c6ef084f27188bd2199
Nicolas Perrenoud <mail@nicu.ch>
2014-11-02 11:20:51 +0100

    Fix div 0 error

--- htdocs/classes/tasks/calculaterankingtask.class.php
+++ htdocs/classes/tasks/calculaterankingtask.class.php
@@ -7,7 +7,7 @@
 		function run()
 		{
 			$num = Ranking::calc();
-			$d = $num[1]/$num[0];
+			$d = $num[0] > 0 ? $num[1]/$num[0] : 0;
 			return "Die Punkte von ".$num[0]." Spielern wurden aktualisiert; ein Spieler hat durchschnittlich ".nf($d)." Punkte";
 		}
 		
