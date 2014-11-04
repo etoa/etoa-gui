afe22094e97ec7be376866d9951574beca555b8e
Nicolas Perrenoud <mail@nicu.ch>
2014-11-05 00:35:42 +0100

    Removed legacy designs

--- htdocs/designs/official/Dark/scripts.js
+++
@@ -1,6 +0,0 @@
-// Tippmessage-Stil
-//The Style array parameters come in the following order
-//stl=[titleColor,TitleBgColor,TitleBgImag,TitleTextAlign,TitleFontFace,TitleFontSize,TextColor,TextBgColor,TextBgImag,TextTextAlign,TextFontFace,TextFontSize,Width,Height,BorderSize,BorderColor,Textpadding,transition number,Transition duration,Transparency level,shadow type,shadow color,Appearance behavior,TipPositionType,Xpos,Ypos]
-stl=["white","#333333","","","",,"white","#7B7B80","","","",,,,2,"#000000",2,,,,,"",,,,]
-
-window.design = "dark";
