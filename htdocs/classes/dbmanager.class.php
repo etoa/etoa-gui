362f0d517d831e13f69ab963bd3f8203d1f8e3d6
MrCage <mrcage@etoa.ch>
2014-11-15 11:16:48 +0100

    Added database export/import methods which can be used if mysql command line tools are not available

--- htdocs/classes/dbmanager.class.php
+++ htdocs/classes/dbmanager.class.php
@@ -399,6 +399,10 @@ class DBManager implements ISingleton	{
 			}
 			else
 			{
+				if (WINDOWS && !file_exists($mysqldump)) {
+					$this->dumpIntoFile($file);
+					return;
+				}
 				$cmd = $mysqldump." -u".$this->getUser()." -p".$this->getPassword()." -h".$this->getHost()." ".$this->getDbName()." > ".$file;
 			}
 			$result = shell_exec($cmd);
@@ -466,6 +470,10 @@ class DBManager implements ISingleton	{
 			}
 			else
 			{
+				if (WINDOWS && !file_exists($mysql)) {
+					$this->importFromFile($file);
+					return;
+				}
 				$cmd = $mysql." -u".$this->getUser()." -p".$this->getPassword()." -h".$this->getHost()." ".$this->getDbName()." < ".$file;
 			}
 			$result = shell_exec($cmd);
@@ -478,6 +486,91 @@ class DBManager implements ISingleton	{
 		{
 			throw new Exception("File $file not found!");	
 		}
+	}
+	
+	/**
+	* Import SQL file using PHP functionality only
+	*/
+	private function importFromFile($file, $delimiter = ';')
+	{
+		set_time_limit(0);
+		if (is_file($file) === true)
+		{
+			$file = fopen($file, 'r');
+			if (is_resource($file) === true)
+			{
+				$query = array();
+				while (feof($file) === false)
+				{
+					$query[] = fgets($file);
+					if (preg_match('~' . preg_quote($delimiter, '~') . '\s*$~iS', end($query)) === 1)
+					{
+						$query = trim(implode('', $query));
+						$this->query($query, 1);
+					}
+					if (is_string($query) === true)
+					{
+						$query = array();
+					}
+				}
+				return fclose($file);
+			}
+		}
+		return false;
+	}
+	
+	/**
+	* Import database or tables to SQL file using PHP functionality only
+	*/
+	private function dumpIntoFile($file, $tables = '*')
+	{
+		//get all of the tables
+		if($tables == '*')
+		{
+			$tables = array();
+			$result = $this->query('SHOW TABLES');
+			while($row = mysql_fetch_row($result))
+			{
+				$tables[] = $row[0];
+			}
+		}
+		else
+		{
+			$tables = is_array($tables) ? $tables : explode(',',$tables);
+		}
+		
+		//cycle through
+		foreach($tables as $table)
+		{
+			$result = $this->query('SELECT * FROM '.$table);
+			$num_fields = mysql_num_fields($result);
+			
+			$return.= 'DROP TABLE '.$table.';';
+			$row2 = mysql_fetch_row($this->query('SHOW CREATE TABLE '.$table));
+			$return.= "\n\n".$row2[1].";\n\n";
+			
+			for ($i = 0; $i < $num_fields; $i++) 
+			{
+				while($row = mysql_fetch_row($result))
+				{
+					$return.= 'INSERT INTO '.$table.' VALUES(';
+					for($j=0; $j<$num_fields; $j++) 
+					{
+						$row[$j] = addslashes($row[$j]);
+						$row[$j] = ereg_replace("\n","\\n",$row[$j]);
+						if (isset($row[$j])) { $return.= '"'.$row[$j].'"' ; } else { $return.= '""'; }
+						if ($j<($num_fields-1)) { $return.= ','; }
+					}
+					$return.= ");\n";
+				}
+			}
+			$return.="\n\n\n";
+		}
+		
+		//save file
+		$handle = fopen($file, 'w+');
+		fwrite($handle,$return);
+		fclose($handle);
 	}
 	
 	public function getBackupImages($dir, $strip=1)
