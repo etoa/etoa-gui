f280a3bc04ab2ab40d03a9a7bcc768f241a9d30d
MrCage <mrcage@etoa.ch>
2014-11-18 20:24:18 +0100

    Re-added image check functionality

--- htdocs/admin/content/misc/imagepacks.inc.php
+++ htdocs/admin/content/misc/imagepacks.inc.php
@@ -1,5 +1,4 @@
 <?PHP
-	$tpl->setView('imagepacks');
 	$tpl->assign('title', 'Bildpakete verwalten');
 
 	$imPackDir = IMAGEPACK_DIRECTORY;
@@ -7,103 +6,98 @@
 	
 	$imagepacks = get_imagepacks();
 	
+	//
+	// Check
+	//
 	if (isset($_GET['manage']))
 	{
-		if (is_dir($imPackDir."/".$_GET['manage']))
+		$tpl->setView('imagepacks');
+		
+		if (isset($imagepacks[$_GET['manage']])) 
 		{
-			$cdir = $imPackDir."/".$_GET['manage'];
-			if ($xml = simplexml_load_file($cdir."/imagepack.xml"))
+			$imagepack = $imagepacks[$_GET['manage']];
+			$cdir = $imagepack['dir'];
+			$exts = $imagepack['extensions'];
+			
+			$sizes = array(
+				"" => $cfg->value('imagesize'),
+				"_middle" => $cfg->p1('imagesize'),
+				"_small" => $cfg->p2('imagesize')
+			);
+
+			$dira = array(
+				"abuildings" => array("building", DBManager::getInstance()->getArrayFromTable("alliance_buildings","alliance_building_id")),
+				"atechnologies" => array("technology", DBManager::getInstance()->getArrayFromTable("alliance_technologies","alliance_tech_id")),
+				"buildings" => array("building", DBManager::getInstance()->getArrayFromTable("buildings","building_id")),
+				"defense" => array("def", DBManager::getInstance()->getArrayFromTable("defense","def_id")),
+				"missiles" => array("missile", DBManager::getInstance()->getArrayFromTable("missiles","missile_id")),
+				"ships" => array("ship", DBManager::getInstance()->getArrayFromTable("ships","ship_id")),
+				"stars" => array("star", DBManager::getInstance()->getArrayFromTable("sol_types","sol_type_id")),
+				"technologies" => array("technology", DBManager::getInstance()->getArrayFromTable("technologies","tech_id")),
+				"nebulas" => array("nebula",range(1,$cfg->value('num_nebula_images'))),
+				"asteroids" => array("asteroids",range(1,$cfg->value('num_asteroid_images'))),
+				"space" => array("space",range(1,$cfg->value('num_space_images'))),
+				"wormholes" => array("wormhole",range(1,$cfg->value('num_wormhole_images'))),
+				"races" => array("race", DBManager::getInstance()->getArrayFromTable("races","race_id")),
+			);
+			
+			foreach ($dira as $sdir => $sd)
 			{
-				echo "<h3>".$xml->name."</h3>";
-				echo "Autor: ".$xml->author." (".$xml->email.")<br/><br/>";
-
-				$tmpexts = explode(",",$xml->extensions);
-				$exts = array();
-				foreach ($tmpexts as $tmpext)
+				$sprefix = $sd[0];
+				if (is_dir($cdir."/".$sdir))
 				{
-					if ($tmpext=="png") $exts[] = "png";
-					if ($tmpext=="jpeg") $exts[] = "jpg";
-					if ($tmpext=="jpg") $exts[] = "jpg";
-					if ($tmpext=="gif") $exts[] = "gif";
-				}
-				if (count($exts) == 0) $exts[] = $baseType;
-
-				$sizes = array("" => $cfg->value('imagesize'),"_middle" => $cfg->p1('imagesize'),"_small" => $cfg->p2('imagesize'));
-
-				$dira = array(
-					"abuildings" => array("building", DBManager::getInstance()->getArrayFromTable("alliance_buildings","alliance_building_id")),
-					"atechnologies" => array("technology", DBManager::getInstance()->getArrayFromTable("alliance_technologies","alliance_tech_id")),
-					"buildings" => array("building", DBManager::getInstance()->getArrayFromTable("buildings","building_id")),
-					"defense" => array("def", DBManager::getInstance()->getArrayFromTable("defense","def_id")),
-					"missiles" => array("missile", DBManager::getInstance()->getArrayFromTable("missiles","missile_id")),
-					"ships" => array("ship", DBManager::getInstance()->getArrayFromTable("ships","ship_id")),
-					"stars" => array("star", DBManager::getInstance()->getArrayFromTable("sol_types","sol_type_id")),
-					"technologies" => array("technology", DBManager::getInstance()->getArrayFromTable("technologies","tech_id")),
-					"nebulas" => array("nebula",range(1,$cfg->value('num_nebula_images'))),
-					"asteroids" => array("asteroids",range(1,$cfg->value('num_asteroid_images'))),
-					"space" => array("space",range(1,$cfg->value('num_space_images'))),
-					"wormholes" => array("wormhole",range(1,$cfg->value('num_wormhole_images'))),
-					"races" => array("race", DBManager::getInstance()->getArrayFromTable("races","race_id")),
-				);
-
-				foreach ($dira as $sdir => $sd)
-				{
-					$sprefix = $sd[0];
-					if (is_dir($cdir."/".$sdir))
+					foreach ($sd[1] as $idx)
 					{
-						foreach ($sd[1] as $idx)
+						$baseFileStr = $sdir."/".$sprefix.$idx.".".$baseType;
+						$baseFile = $cdir."/".$baseFileStr;
+						if (!is_file($baseFile))
 						{
-							$baseFileStr = $sdir."/".$sprefix.$idx.".".$baseType;
-							$baseFile = $cdir."/".$baseFileStr;
-							if (!is_file($baseFile))
+							echo "<i>Basisbild fehlt: $baseFile</i><br/>";
+						}
+						else
+						{
+							foreach ($exts as $ext)
 							{
-								echo "<i>Basisbild fehlt: $baseFile</i><br/>";
-							}
-							else
-							{
-								foreach ($exts as $ext)
+								foreach ($sizes as $sizep => $sizew)
 								{
-									foreach ($sizes as $sizep => $sizew)
+									$filestr = $sdir."/".$sprefix.$idx.$sizep.".".$ext;
+									$file = $cdir."/".$filestr;
+									if (is_file($file))
 									{
-										$filestr = $sdir."/".$sprefix.$idx.$sizep.".".$ext;
-										$file = $cdir."/".$filestr;
-										if (is_file($file))
+										$sa = getimagesize($file);
+										if ($sa[0] != $sizew)
 										{
-											$sa = getimagesize($file);
-											if ($sa[0] != $sizew)
-											{
-												echo "Falsche Grösse: <i>$filestr</i> (".$sa[0]." statt $sizew).";
-												if (resizeImage($file, $file, $sizew,$sizew, $ext))
-													echo "<span style=\"color:#0f0;\">KORRIGIERT!</span>";
-												echo "<br/>";
-											}
-										}
-										else
-										{
-											echo "<i>Fehlt: $filestr</i>";
-											if (resizeImage($baseFile, $file, $sizew,$sizew, $ext))
+											echo "Falsche Grösse: <i>$filestr</i> (".$sa[0]." statt $sizew).";
+											if (resizeImage($file, $file, $sizew,$sizew, $ext))
 												echo "<span style=\"color:#0f0;\">KORRIGIERT!</span>";
 											echo "<br/>";
 										}
 									}
+									else
+									{
+										echo "<i>Fehlt: $filestr</i>";
+										if (resizeImage($baseFile, $file, $sizew,$sizew, $ext))
+											echo "<span style=\"color:#0f0;\">KORRIGIERT!</span>";
+										echo "<br/>";
+									}
 								}
 							}
 						}
-					}		
-					else
-					{
-						echo "Verzeichnis fehlt: $sdir<br/>";
-					}				
-				}
-
-
-				echo button("Zurück","?page=$page&amp;sub=$sub");
-				
+					}
+				}		
+				else
+				{
+					echo "Verzeichnis fehlt: $sdir<br/>";
+				}				
 			}
+			echo button("Zurück","?page=$page&amp;sub=$sub");
 		}
 		
 	}
+	
+	//
 	// Imagepack download
+	//
 	else if (!empty($_GET['download']))
 	{
 		$imagepack = $_GET['download'];
@@ -124,9 +118,14 @@
 				$tpl->assign('errmsg', $e->getMessage());
 			}
 		}
-	}	
+	}
+	
+	//
+	// Overview
+	//
 	else
 	{
+		$tpl->setView('imagepacks');
 		$tpl->assign('imagepacks', $imagepacks);
 	}	
 ?>
