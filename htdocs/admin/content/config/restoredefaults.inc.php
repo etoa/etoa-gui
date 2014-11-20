3d6135f097e5f48d81ff42e56abf8f8599fd7969
MrCage <mrcage@etoa.ch>
2014-11-20 19:10:52 +0100

    Enhanced restore config defaults table

--- htdocs/admin/content/config/restoredefaults.inc.php
+++ htdocs/admin/content/config/restoredefaults.inc.php
@@ -6,13 +6,60 @@
 	{
 		if ($cnt = $cfg->restoreDefaults())
 		{
-			ok_msg("$cnt Einstellungen wurden wiederhergestellt!");
+			$tpl->assign("msg", "$cnt Einstellungen wurden wiederhergestellt!");
 			BackendMessage::reloadConfig();
 		}
 	}
-
-	echo "<form action=\"?page=$page&amp;sub=$sub\" method=\"post\">";
-	echo "<p>Soll die Konfigurationstabelle wirklich auf ihre Standardwerte zurückgesetzt werden?</p>";
-	echo "<p><input type=\"submit\" name=\"restoresubmit\" value=\"Ja, Einstellungen zurücksetzen\" /></p>";
-	echo "</form>";
+	
+	// Changed values
+	foreach ($cfg->categories() as $ck => $cv) 
+	{
+		foreach ($cfg->itemInCategory($ck) as $i)
+		{
+			$name = $i['name'];
+			if (isset($i->v))
+			{
+				if ((string)$i->v != $cfg->$name->v)
+				{
+					$items[] = array(
+						'category' => $cv,
+						'label' => (string)$i->v['comment'],
+						'name' => (string)$i['name'],
+						'type' => 'v',
+						'value' => $cfg->$name->v,
+						'default' => (string)$i->v,
+					);
+				}
+			}
+			if (isset($i->p1))
+			{
+				if ((string)$i->p1 != $cfg->$name->p1)
+				{
+					$items[] = array(
+						'category' => $cv,
+						'label' => (string)$i->p1['comment'],
+						'name' => (string)$i['name'],
+						'type' => 'p1',
+						'value' => $cfg->$name->p1,
+						'default' => (string)$i->p1,
+					);
+				}
+			}
+			if (isset($i->p2))
+			{
+				if ((string)$i->p2 != $cfg->$name->p2)
+				{
+					$items[] = array(
+						'category' => $cv,
+						'label' => (string)$i->p2['comment'],
+						'name' => (string)$i['name'],
+						'type' => 'p2',
+						'value' => $cfg->$name->p2,
+						'default' => (string)$i->p2,
+					);
+				}
+			}
+		}
+    }
+	$tpl->assign("changedValues", $items);
 ?>
