6a1dd5d8f963717a614caa1c9bb7aa65725432fc
coo1ness <sebastian.rieger89@gmail.com>
2014-11-26 05:36:50 +0100

    sql-file for energy bug

---
+++ db/migrations/0002-26112014-energiebugfix.sql
@@ -0,0 +1,8 @@
+UPDATE `etoa`.`buildings` SET `building_costs_power` = '10' WHERE `buildings`.`building_id` = 1;
+UPDATE `etoa`.`buildings` SET `building_costs_power` = '20' WHERE `buildings`.`building_id` = 2;
+UPDATE `etoa`.`buildings` SET `building_costs_power` = '20' WHERE `buildings`.`building_id` = 3;
+UPDATE `etoa`.`buildings` SET `building_costs_power` = '50' WHERE `buildings`.`building_id` = 4;
+UPDATE `etoa`.`buildings` SET `building_costs_power` = '5' WHERE `buildings`.`building_id` = 5;
+UPDATE `etoa`.`buildings` SET `building_costs_power` = '50' WHERE `buildings`.`building_id` = 6;
+UPDATE `etoa`.`buildings` SET `building_costs_power` = '100' WHERE `buildings`.`building_id` = 22;
+UPDATE `etoa`.`buildings` SET `building_costs_power` = '50000' WHERE `buildings`.`building_id` = 25;
