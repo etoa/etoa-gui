f8abc85592ddf6eb210230796a454a8546db8935
MrCage <mrcage@etoa.ch>
2014-12-01 22:14:08 +0100

    Removed db name from schemas

--- db/migrations/0002-26112014-energiebugfix.sql
+++ db/migrations/0002-26112014-energiebugfix.sql
@@ -1,8 +1,8 @@
-UPDATE `etoa_test`.`buildings` SET `building_costs_power` = '10' WHERE `buildings`.`building_id` = 1;
-UPDATE `etoa_test`.`buildings` SET `building_costs_power` = '20' WHERE `buildings`.`building_id` = 2;
-UPDATE `etoa_test`.`buildings` SET `building_costs_power` = '20' WHERE `buildings`.`building_id` = 3;
-UPDATE `etoa_test`.`buildings` SET `building_costs_power` = '50' WHERE `buildings`.`building_id` = 4;
-UPDATE `etoa_test`.`buildings` SET `building_costs_power` = '5' WHERE `buildings`.`building_id` = 5;
-UPDATE `etoa_test`.`buildings` SET `building_costs_power` = '50' WHERE `buildings`.`building_id` = 6;
-UPDATE `etoa_test`.`buildings` SET `building_costs_power` = '100' WHERE `buildings`.`building_id` = 22;
-UPDATE `etoa_test`.`buildings` SET `building_costs_power` = '50000' WHERE `buildings`.`building_id` = 25;
+UPDATE `buildings` SET `building_costs_power` = '10' WHERE `buildings`.`building_id` = 1;
+UPDATE `buildings` SET `building_costs_power` = '20' WHERE `buildings`.`building_id` = 2;
+UPDATE `buildings` SET `building_costs_power` = '20' WHERE `buildings`.`building_id` = 3;
+UPDATE `buildings` SET `building_costs_power` = '50' WHERE `buildings`.`building_id` = 4;
+UPDATE `buildings` SET `building_costs_power` = '5' WHERE `buildings`.`building_id` = 5;
+UPDATE `buildings` SET `building_costs_power` = '50' WHERE `buildings`.`building_id` = 6;
+UPDATE `buildings` SET `building_costs_power` = '100' WHERE `buildings`.`building_id` = 22;
+UPDATE `buildings` SET `building_costs_power` = '50000' WHERE `buildings`.`building_id` = 25;
