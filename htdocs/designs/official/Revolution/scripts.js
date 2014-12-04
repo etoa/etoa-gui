ff40071bac18fb830677ca2eec1185b0bcb47b52
MrCage <mrcage@etoa.ch>
2014-12-04 23:07:12 +0100

    Added Revolution Design by HeaX & Lukulus, Version 0.91 B

---
+++ htdocs/designs/official/Revolution/scripts.js
@@ -0,0 +1,50 @@
+/* 
+   ETOA Design Revolution by HeaX	
+	umgesetzt durch :	ETOA-Team - Lukulus
+	
+	Stand: 02. Dezember 14
+	Version: 0.9 Beta
+*/
+
+/* Eigene Scripte */
+
+$(document).ready(function() {
+
+	if (document.getElementById('seitenname').innerHTML == 'overview') {
+		$('.infoboxtitle').hide(); 
+		$('.infoboxcontent').hide();
+	}
+	
+	if (sessionStorage.chat) {
+			$("#c_aside").replaceWith("<iframe id='c_aside' src='chat.php'></iframe>");
+			document.getElementById("f_chat").style.display = "none";
+			document.getElementById("f_chat_on").style.display = "inline";	
+	}	
+});
+
+
+$("#f_chat").click(function(){
+	$("#c_aside").replaceWith("<iframe id='c_aside' src='chat.php'></iframe>");
+	document.getElementById("f_chat").style.display = "none";
+	document.getElementById("f_chat_on").style.display = "inline";
+	sessionStorage.chat = 1;
+	console.log (sessionStorage.chat);
+});
+
+
+$("#f_chat_on").click(function(){
+	sessionStorage.clear();
+	$("#aside").load('{$templateDir}/template.html #c_aside');
+	document.getElementById("f_chat_on").style.display = "none";
+	document.getElementById("f_chat").style.display = "inline";
+});
+
+
+function js_planetlist(enable) {
+	if (enable) {
+		document.getElementById('planetlist').style.visibility='visible';
+	}
+	else {
+		document.getElementById('planetlist').style.visibility='hidden';
+	}
+}
