01b014cb184c57c749829098ae72c7f33161ed05
MrCage <mrcage@etoa.ch>
2014-12-09 12:27:09 +0100

    Added dedicated credits page

--- htdocs/content/contact.php
+++ htdocs/content/contact.php
@@ -123,24 +123,5 @@
 		} else {
 			echo "<i>Keine Kontaktpersonen vorhanden!</i>";
 		}		
-
-		echo '<p>Wir danken den folgenden Open-Source Projekten für ihre tolle und wertvolle Arbeit:</p>';
-		$thirdparty = fetchJsonConfig("thirdparty.conf");
-		tableStart();
-		echo '<tr>
-			<th>Projekt</th>
-			<th>Beschreibung</th>
-			<th>Website</th>
-		</tr>';
-		foreach ($thirdparty as $tp) {
-			echo '<tr>
-				<td>'.$tp['name'].'</td>
-				<td>'.$tp['description'].'</td>
-				<td><a href="'.$tp['url'].'" target="_blank">'.$tp['url'].'</a></td>
-			</tr>';
-		}
-		tableEnd();
 	}
-?>                                                  
-                                                    
-                                                    
+?>
