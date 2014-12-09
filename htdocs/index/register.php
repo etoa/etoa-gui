b434f4bb06f21ea0e391a52b92a94445c307df81
MrCage <mrcage@etoa.ch>
2014-12-09 16:14:47 +0100

    Improvements of registration screen ui

--- htdocs/index/register.php
+++ htdocs/index/register.php
@@ -52,101 +52,97 @@
 		}
 		else
 		{
-			
 			$userName = isset($_SESSION['REGISTER']['register_user_name']) ? $_SESSION['REGISTER']['register_user_name'] : '';
 			$userNick = isset($_SESSION['REGISTER']['register_user_nick']) ? $_SESSION['REGISTER']['register_user_nick'] : '';
 			$userEmail = isset($_SESSION['REGISTER']['register_user_email']) ? $_SESSION['REGISTER']['register_user_email'] : '';
 			$userPassword = isset($_SESSION['REGISTER']['register_user_password']) ? $_SESSION['REGISTER']['register_user_password'] : '';
-			
-			echo 'Melde dich hier für die '.Config::getInstance()->roundname->v.' von '.APP_NAME.' an. Es sind noch <b>'.max($cfg->p2('enable_register')-$ucnt[0],0).'</b> von <b>'.$cfg->p2('enable_register').'</b> Plätzen frei!<br/><br/>';
-			echo "<form action=\"?index=register\" method=\"post\">
-			<div style=\"width:700px;margin:5px auto;\">";
-
-			tableStart("Anmeldeformular");
-		
-			echo "<tr><th class=\"tbltitle\" style=\"width:150px;\">Vollst&auml;ndiger Name:</th>";
-			echo "<td class=\"tbldata\" style=\"width:170px;\">
-				<input type=\"text\" id=\"register_user_name\" name=\"register_user_name\" maxlength=\"".NAME_MAXLENGTH."\" size=\"".NAME_MAXLENGTH."\" value=\"".$userName."\"  autocomplete=\"off\" /></td>";
-			echo "<td class=\"tbldata\">Hier musst du deinen realen Namen angeben; dies dient zur Kontrolle gegen Multis. Dieser Name ist nur f&uuml;r Administratoren sichtbar!<br/><span id=\"nameStatus\"></span></td></tr>";
-			
-			echo "<tr><th class=\"tbltitle\">E-Mail:</th>";
-			echo "<td class=\"tbldata\">
-				<input type=\"text\" id=\"register_user_email\" name=\"register_user_email\" maxlength=\"50\" size=\"30\" value=\"".$userEmail."\"  autocomplete=\"off\" /></td>";
-			echo "<td class=\"tbldata\">Du musst eine g&uuml;ltige E-Mail-Adresse eingeben. Auf diese wird dir ein Passwort zugeschickt mit dem du dich einloggen kannst.<br/><span id=\"emailStatus\"></span></td></tr>";
-
-			echo "<tr><th class=\"tbltitle\">Benutzername:</th>";
-			echo "<td class=\"tbldata\">
-				<input type=\"text\" id=\"register_user_nick\" name=\"register_user_nick\" maxlength=\"".NICK_MAXLENGHT."\" size=\"".NICK_MAXLENGHT."\" value=\"".$userNick."\" autocomplete=\"off\" /></td>";
-			echo "<td class=\"tbldata\">Mit diesem Name tritts du im Spiel als der Herrscher deines Volkes auf. <b>Der Nickname ist endgültig und kann nicht geändert werden!</b><br/><span id=\"nickStatus\"></span></td></tr>";
-
-			echo "<tr><th class=\"tbltitle\">Passwort:</th>";
-			echo "<td class=\"tbldata\">
-				<input type=\"password\" id=\"register_user_password\" name=\"register_user_password\" size=\"20\" value=\"".$userPassword."\" autocomplete=\"off\" /></td>";
-			echo "<td class=\"tbldata\">Wähle ein sicheres Passwort damit niemand unbefugt in deinen Account einloggen kann.</b><br/><span id=\"passwordStatus\"></span></td></tr>";
-
-			
-			echo "<tr><td colspan=\"3\">
-			<input type=\"checkbox\" name=\"agbread\" id=\"agbread\" value=\"1\" />
-			<label for=\"agbread\">Ich akzeptiere die <a href=\"javascript:;\" onclick=\"window.open('".RULES_URL."');\" >Regeln</a>
-			sowie die <a href=\"javascript:;\" onclick=\"window.open('".PRIVACY_URL."');\" >Datenschutzerklärung</a></label>
-			<br/><ul style=\"text-align:left;margin-left:30px\">
-			<li>Pro Person darf nur 1 Account verwendet werden. Multis werden rigoros <a href=\"javascript:;\" onclick=\"window.open('?index=pillory');\">gesperrt</a>!</li>
-			<li>Nach der Registration wird ein automatisch generiertes Passwort an die angegebene E-Mail-Adresse gesendet.</li>
-			<li>Der Name und die E-Mail-Adresse können nur von den Game-Administratoren eingesehen werden und werden nicht weitergegeben.</li>
-			</ul>
-
-			</td></tr>";
-			echo "</table><br/>";
 			?>
+			
+			<p>Melde dich hier für die <?=Config::getInstance()->roundname->v?> von <?=APP_NAME?> an. 
+			
+			<form action="?index=register" method="post">
+				<div style="width:700px;margin:5px auto;">
+					<?PHP
+					tableStart("Anmeldeformular");
+				
+					echo "<tr><th class=\"tbltitle\" style=\"width:150px;\">Vollst&auml;ndiger Name:</th>";
+					echo "<td class=\"tbldata\" style=\"width:170px;\">
+						<input type=\"text\" id=\"register_user_name\" name=\"register_user_name\" maxlength=\"".NAME_MAXLENGTH."\" size=\"".NAME_MAXLENGTH."\" value=\"".$userName."\"  autocomplete=\"off\" /></td>";
+					echo "<td class=\"tbldata\">Hier musst du deinen realen Namen angeben; dies dient zur Kontrolle gegen Multis. Dieser Name ist nur f&uuml;r Administratoren sichtbar!<br/><span id=\"nameStatus\"></span></td></tr>";
+					
+					echo "<tr><th class=\"tbltitle\">E-Mail:</th>";
+					echo "<td class=\"tbldata\">
+						<input type=\"text\" id=\"register_user_email\" name=\"register_user_email\" maxlength=\"50\" size=\"30\" value=\"".$userEmail."\"  autocomplete=\"off\" /></td>";
+					echo "<td class=\"tbldata\">Du musst eine g&uuml;ltige E-Mail-Adresse eingeben. Auf diese wird dir ein Passwort zugeschickt mit dem du dich einloggen kannst.<br/><span id=\"emailStatus\"></span></td></tr>";
 
-			<input type="submit" id="register_submit" disabled="disabled" name="register_submit" value="Anmelden!" /> &nbsp; 
-			<a href="?index=login">Zurück zum Login</a>
-			</div>
+					echo "<tr><th class=\"tbltitle\">Benutzername:</th>";
+					echo "<td class=\"tbldata\">
+						<input type=\"text\" id=\"register_user_nick\" name=\"register_user_nick\" maxlength=\"".NICK_MAXLENGHT."\" size=\"".NICK_MAXLENGHT."\" value=\"".$userNick."\" autocomplete=\"off\" /></td>";
+					echo "<td class=\"tbldata\">Mit diesem Name tritts du im Spiel als der Herrscher deines Volkes auf. <b>Der Nickname ist endgültig und kann nicht geändert werden!</b><br/><span id=\"nickStatus\"></span></td></tr>";
+
+					echo "<tr><th class=\"tbltitle\">Passwort:</th>";
+					echo "<td class=\"tbldata\">
+						<input type=\"password\" id=\"register_user_password\" name=\"register_user_password\" size=\"20\" value=\"".$userPassword."\" autocomplete=\"off\" /></td>";
+					echo "<td class=\"tbldata\">Wähle ein sicheres Passwort damit niemand unbefugt in deinen Account einloggen kann.</b><br/><span id=\"passwordStatus\"></span></td></tr>";
+					?>
+					
+					<tr><td colspan="3">
+					<input type="checkbox" name="agbread" id="agbread" value="1" />
+					<label for="agbread">Ich akzeptiere die <a href="javascript:;" onclick="window.open('<?=RULES_URL?>');" >Regeln</a>
+					sowie die <a href="javascript:;" onclick="window.open('<?=PRIVACY_URL?>');" >Datenschutzerklärung</a></label>
+					<br/>
+					<ul style="text-align:left;margin-left:30px">
+						<li>Pro Person darf nur 1 Account verwendet werden. Multis werden rigoros gesperrt</a>!</li>
+						<li>Nach der Registration wird ein automatisch generiertes Passwort an die angegebene E-Mail-Adresse gesendet.</li>
+						<li>Der Name und die E-Mail-Adresse können nur von den Game-Administratoren eingesehen werden und werden nicht weitergegeben.</li>
+					</ul>
+					Wenn du Hilfe benötigst, kannst du <a href="?index=contact">hier</a> einen Game-Admin kontaktieren.					
+					</td></tr>
+					</table>		
+
+					<input type="submit" id="register_submit" disabled="disabled" name="register_submit" value="Anmelden!" /> &nbsp; 
+					<a href="?index=login">Zurück zum Login</a>
+				</div>
 			</form>
 
-				<script type="text/javascript">
-					$(function(){
-						$('#register_user_name').focus();
-						
-						$('#register_user_name').keyup(function(){
+			<script type="text/javascript">
+				$(function(){
+					$('#register_user_name').focus();
+					
+					$('#register_user_name').keyup(function(){
+						if (this.value) {
 							xajax_registerCheckName(this.value);
-						});
-						$('#register_user_name').blur(function(){
-							xajax_registerCheckName(this.value);
-						});
-						
-						$('#register_user_email').keyup(function(){
-							xajax_registerCheckEmail(this.value);
-						});
-						$('#register_user_email').blur(function(){
-							xajax_registerCheckEmail(this.value);
-						});
-						
-						$('#register_user_nick').keyup(function(){
-							xajax_registerCheckNick(this.value);
-						});
-						$('#register_user_nick').blur(function(){
-							xajax_registerCheckNick(this.value);
-						});
-						
-						$('#register_user_password').keyup(function(){
-							xajax_registerCheckPassword(this.value);
-						});
-						$('#register_user_password').blur(function(){
-							xajax_registerCheckPassword(this.value);
-						});
-						
-						$('#agbread').click(function(){
-							if (this.checked) 
-								$('#register_submit').prop('disabled', false); 
-							else 
-								$('#register_submit').prop('disabled', true); 
-						});
+						}
 					});
-				</script>			
+					
+					$('#register_user_email').keyup(function(){
+						if (this.value) {
+							xajax_registerCheckEmail(this.value);
+						}
+					});
+					
+					$('#register_user_nick').keyup(function(){
+						if (this.value) {
+							xajax_registerCheckNick(this.value);
+						}
+					});
+					
+					$('#register_user_password').keyup(function(){
+						if (this.value) {
+							xajax_registerCheckPassword(this.value);
+						}
+					});
+					
+					$('#agbread').click(function(){
+						if (this.checked) 
+							$('#register_submit').prop('disabled', false); 
+						else 
+							$('#register_submit').prop('disabled', true); 
+					});
+				});
+			</script>			
 			<?PHP
 		}		
-		
 	}
 
 	// BEGIN SCRIPT //
