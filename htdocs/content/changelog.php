41575b7e6dee41df6afa4e876f3bc69b72342152
MrCage <mrcage@etoa.ch>
2014-12-09 09:04:14 +0100

    Left-aligned changelog texts

--- htdocs/content/changelog.php
+++ htdocs/content/changelog.php
@@ -1,8 +1,10 @@
 <?PHP
 	$changelogFile = "../Changelog_public.md";
 	if (is_file($changelogFile)) {
+		echo '<div style="text-align:left;">';
 		$Parsedown = new Parsedown();
 		echo $Parsedown->text(file_get_contents($changelogFile)); 
+		echo '</div>';
 	} else {
 		echo "<h1>Changelog</h1>";
 		error_msg("Changelog nicht verfügbar!",1);
