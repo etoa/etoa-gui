5f3c0860155bf88f9dfda2332adb31e0045fdada
MrCage <mrcage@etoa.ch>
2014-12-11 00:09:07 +0100

    Removed db name in schema

--- db/migrations/0005-20141210-remove_test_ships.sql
+++ db/migrations/0005-20141210-remove_test_ships.sql
@@ -1,2 +1,2 @@
-DELETE FROM `etoa_test`.`ships` WHERE `ships`.`ship_id` = 90 LIMIT 1;
-DELETE FROM `etoa_test`.`ships` WHERE `ships`.`ship_id` = 85 LIMIT 1;
+DELETE FROM `ships` WHERE `ships`.`ship_id` = 90 LIMIT 1;
+DELETE FROM `ships` WHERE `ships`.`ship_id` = 85 LIMIT 1;
