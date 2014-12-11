14ed088be22bc7ac2287d420de8cff17b19ad14a
MrCage <mrcage@etoa.ch>
2014-12-11 09:35:55 +0100

    UTF 8 encoding of db migration schemas

--- db/migrations/0003-30112014-dual_control.sql
+++ db/migrations/0003-30112014-dual_control.sql
@@ -1,3 +1,3 @@
 ALTER TABLE `users` ADD `dual_email` VARCHAR(50) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL;
 ALTER TABLE `users` ADD `dual_name` VARCHAR(30) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL;
-INSERT INTO `ticket_cat` (`name`, `sort`) VALUES ('Änderung meines Dualspielers', '8');
+INSERT INTO `ticket_cat` (`name`, `sort`) VALUES ('Ã„nderung meines Dualspielers', '8');
