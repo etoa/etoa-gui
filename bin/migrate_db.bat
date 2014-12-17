7c2fb816c0f66c450e038dc45fc5f43e0300c830
MrCage <mrcage@etoa.ch>
2014-12-17 23:13:06 +0100

    Added windows helper script for DB migration

---
+++ bin/migrate_db.bat
@@ -0,0 +1,18 @@
+@echo off
+
+if exist c:\xampp\php\php.exe (
+	set php=c:\xampp\php\php.exe
+	goto exec
+) 
+if exist d:\xampp\php\php.exe (
+	set php=d:\xampp\php\php.exe
+	goto exec
+)
+echo Unable to find PHP
+goto done
+
+:exec
+%php% db.php migrate
+
+:done
+pause
