f6bc05225d00166a16fc8b6fecf475da2ee28d61
Stephan Vock <stephan.vock@gmail.com>
2014-12-24 00:01:14 +0100

    PHP 5.3

--- src/app.php
+++ src/app.php
@@ -5,10 +5,10 @@ require_once dirname(__DIR__) . '/vendor/autoload.php';
 $environment = 'development';
 
 $app = new \Silex\Application(
-    [
+    array(
         'app.environment' => $environment,
         'app.root_dir'      => dirname(__DIR__),
-    ]
+    )
 );
 
 require dirname(__DIR__) . '/config/config.php';
