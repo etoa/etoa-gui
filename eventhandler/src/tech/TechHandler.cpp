ec40da065485bcdbc5797727a93988b275256b37
MrCage <mrcage@etoa.ch>
2015-01-07 21:45:07 +0100

    When updating technologies, remember affected users

--- eventhandler/src/tech/TechHandler.cpp
+++ eventhandler/src/tech/TechHandler.cpp
@@ -7,8 +7,39 @@ namespace tech
 	{
 		std::time_t time = std::time(0);
 		
-		// Load planets who needs updating
 		mysqlpp::Query query = con_->query();
+		
+		// Load users who needs updating
+		query << "SELECT "
+			<< "	techlist_user_id "
+			<< "FROM "
+			<< "	techlist "
+			<< "WHERE "
+			<< "	techlist_build_type=3 "
+			<< "	AND techlist_build_end_time<" << time << " "
+			<< "ORDER BY "
+			<< "	techlist_user_id;";
+		RESULT_TYPE res = query.store();
+		
+		// Add changed users to vector
+		if (res) {
+			unsigned int resSize = res.size();
+			if (resSize>0) {
+				this->changes_ = true;
+				mysqlpp::Row row;
+				int lastId = 0;
+				
+				for (mysqlpp::Row::size_type i = 0; i<resSize; i++) {
+					row = res.at(i);
+					int uid = (int)row["techlist_user_id"];
+					// Make sure there are no duplicate planet id's
+					if (uid!=lastId) {
+						this->changedUsers_.push_back(uid);
+					}
+					lastId = uid;
+				}
+			}
+		}
 		
 		// Perform level update
 		query << "UPDATE "
