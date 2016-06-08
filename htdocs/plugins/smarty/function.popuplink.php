eabd37fe4264214c4b10cd89836b8450008bd912
Stephan Vock <stephan.vock@gmail.com>
2016-06-08 22:34:47 +0200

    Fix: smarty redeclare function error on setup

--- htdocs/plugins/smarty/function.popuplink.php
+++ htdocs/plugins/smarty/function.popuplink.php
@@ -1,6 +1,7 @@
 <?PHP
-function smarty_function_popuplink($params, $template)
-{
-	return popupLink($params["type"], $params["title"], isset($params['class']) ? $params['class'] : null, isset($params['params']) ? $params['css'] : null);
+if (!function_exists('smarty_function_popuplink')) {
+	function smarty_function_popuplink($params, $template)
+	{
+		return popupLink($params["type"], $params["title"], isset($params['class']) ? $params['class'] : null, isset($params['params']) ? $params['css'] : null);
+	}
 }
-?>
