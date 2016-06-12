3c73c4b779c56ffb1da48b6212e43aaa9ad5de8e
coo1ness <sebastian.rieger89@gmail.com>
2016-06-12 21:39:00 +0200

    fix for workers and fetch

--- htdocs/classes/buildlistitem.class.php
+++ htdocs/classes/buildlistitem.class.php
@@ -301,7 +301,7 @@
 			if ($this->id>0)
 			{
 				dbquery("UPDATE buildlist SET buildlist_build_type='3', buildlist_build_start_time='".$this->startTime.
-					"', buildlist_build_end_time='".$this->endTime."', buildlist_people_working_status = '1' WHERE buildlist_id='".$this->id."' LIMIT 1;");
+					"', buildlist_build_end_time='".$this->endTime."' WHERE buildlist_id='".$this->id."' LIMIT 1;");
 			}
 			else
 			{
@@ -315,7 +315,6 @@
 							buildlist_current_level,
 							buildlist_build_start_time,
 							buildlist_build_end_time,
-							buildlist_people_working_status
 						)
 						VALUES
 						(
@@ -325,10 +324,21 @@
 							'".$this->buildType."',
 							'".$this->level."',
 							'".$this->startTime."',
-							'".floor($this->endTime)."',
-							'1'
+							'".floor($this->endTime)."'
 						);");
 			}
+
+			dbquery("
+				UPDATE
+					buildlist
+				SET
+					buildlist_people_working_status='1'
+				WHERE
+					buildlist_building_id='" . BUILD_BUILDING_ID . "'
+					AND buildlist_user_id='" . $cu->id . "'
+					AND buildlist_entity_id='" . $cp->id . "'");
+
+
 			Buildlist::$underConstruction = true;
 			$cp->changeRes(-$costs['costs0'],-$costs['costs1'],-$costs['costs2'],-$costs['costs3'],-$costs['costs4']);
 
@@ -442,7 +452,18 @@
 				$this->startTime = 0;
 				$this->buildType = 0;
 
-				dbquery("UPDATE buildlist SET buildlist_build_type='0', buildlist_build_start_time='0', buildlist_build_end_time='0', buildlist_people_working_status = '0' WHERE buildlist_id='".$this->id."' LIMIT 1;");
+				dbquery("UPDATE buildlist SET buildlist_build_type='0', buildlist_build_start_time='0', buildlist_build_end_time='0' WHERE buildlist_id='".$this->id."' LIMIT 1;");
+
+				dbquery("
+					UPDATE
+						buildlist
+					SET
+						buildlist_people_working_status='0'
+					WHERE
+						buildlist_building_id='" . DEF_BUILDING_ID . "'
+						AND buildlist_user_id='" . $cu->id . "'
+						AND buildlist_entity_id='" . $cp->id . "'");
+
 				Buildlist::$underConstruction = false;
 				//Rohstoffe vom Planeten abziehen und aktualisieren
 				$cp->changeRes($costs['costs0']*$fac,$costs['costs1']*$fac,$costs['costs2']*$fac,$costs['costs3']*$fac,$costs['costs4']*$fac);
