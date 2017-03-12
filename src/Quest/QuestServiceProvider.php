2adca4487a4a64a0cccf23327cac26c6c0f34445
Stephan Vock <stephan.vock@gmail.com>
2017-03-12 23:52:15 +0100

    Quests: extend setup

--- src/Quest/QuestServiceProvider.php
+++ src/Quest/QuestServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace EtoA\Quest;
 
+use EtoA\Quest\Reward\DefenseRewardCollector;
+use EtoA\Quest\Reward\MissileRewardCollector;
 use EtoA\Quest\Reward\ShipRewardCollector;
 use Pimple\Container;
 use Pimple\ServiceProviderInterface;
@@ -17,7 +19,18 @@ class QuestServiceProvider implements ServiceProviderInterface
             return new ShipRewardCollector($pimple['etoa.ship.repository'], $pimple['etoa.planet.repository']);
         };
         $pimple['etoa.quest.reward.defensecollector'] = function (Container $pimple) {
-            return new ShipRewardCollector($pimple['etoa.defense.repository'], $pimple['etoa.planet.repository']);
+            return new DefenseRewardCollector($pimple['etoa.defense.repository'], $pimple['etoa.planet.repository']);
+        };
+        $pimple['etoa.quest.reward.missilecollector'] = function (Container $pimple) {
+            return new MissileRewardCollector($pimple['etoa.missile.repository'], $pimple['etoa.planet.repository']);
+        };
+
+        $pimple['cubicle.quests.rewards.collectors'] = function (Container $pimple) {
+            return [
+                $pimple['etoa.quest.reward.shipcollector'],
+                $pimple['etoa.quest.reward.defensecollector'],
+                $pimple['etoa.quest.reward.missilecollector'],
+            ];
         };
     }
 }
