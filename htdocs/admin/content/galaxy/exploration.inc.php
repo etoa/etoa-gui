5d43cf314b320ccda99f8bf75408dfde16ab6cf8
Stephan Vock <stephan.vock@gmail.com>
2020-03-25 17:08:27 +0000

    Admin: replace smarty with twig on galaxy exploration page

--- htdocs/admin/content/galaxy/exploration.inc.php
+++ htdocs/admin/content/galaxy/exploration.inc.php
@@ -1,104 +1,106 @@
 <?PHP
-	$tpl->setView('galaxy/exploration');
-	$tpl->assign('title', "Erkundung");
 
-	$res=dbquery("
-	SELECT 
-		user_id,
-		user_nick 
-	FROM 
-		users 
-	ORDER BY 
-		user_nick
-	;");
-	if (mysql_num_rows($res)>0)
-	{
-		$users = [];
-		while ($arr = mysql_fetch_assoc($res))
-		{
-			$users[$arr['user_id']] = $arr['user_nick'];
-		}
-		$tpl->assign("users", $users);
-	}
-	else
-	{
-		$tpl->assign("errmsg", "Keine Benutzer vorhanden!");
-	}
+$errorMessage = null;
+$successMessage = null;
+$users = [];
+$res=dbquery("
+SELECT 
+    user_id,
+    user_nick 
+FROM 
+    users 
+ORDER BY 
+    user_nick
+;");
+if (mysql_num_rows($res)>0) {
+    while ($arr = mysql_fetch_assoc($res)) {
+        $users[$arr['user_id']] = $arr['user_nick'];
+    }
+} else {
+    $errorMessage = 'Keine Benutzer vorhanden!';
+}
 
-    $tpl->assign("uid", 0);
-	if (isset($_GET['user_id']) && $_GET['user_id']>0)
-	{
-		$uid = $_GET['user_id'];
-		$tpl->assign("uid", $uid);
+$uid = null;
+$user = null;
+if (isset($_GET['user_id']) && $_GET['user_id']>0) {
+    $uid = $_GET['user_id'];
 
-		$user = new User($uid);
-		$tpl->assign("user", $user);
+    $user = new User($uid);
 
-		$sx = 1;
-		$sy = 1;
-		$cx = 1;
-		$cy = 1;
-		$radius = 1;
+    $sx = 1;
+    $sy = 1;
+    $cx = 1;
+    $cy = 1;
+    $radius = 1;
 
-		// Discover selected cell
-		if (isset($_POST['discover_selected']))
-		{
-			$sx = intval($_POST['sx']);
-			$sy = intval($_POST['sy']);
-			$cx = intval($_POST['cx']);
-			$cy = intval($_POST['cy']);
-			$radius = abs(intval($_POST['radius']));
+    // Discover selected cell
+    if (isset($_POST['discover_selected'])) {
+        $sx = intval($_POST['sx']);
+        $sy = intval($_POST['sy']);
+        $cx = intval($_POST['cx']);
+        $cy = intval($_POST['cy']);
+        $radius = abs(intval($_POST['radius']));
 
-			$res = dbQuerySave("
-			SELECT
-				id
-			FROM 
-				cells
-			WHERE 
-			 	sx=? 
-				AND sy=? 
-				AND cx=? 
-				AND cy=?;",
-			array(
-				$sx,
-				$sy,
-				$cx,
-				$cy
-			));
-			if (mysql_num_rows($res))
-			{
-				$arr = mysql_fetch_row($res);
-				$cell = new Cell($arr[0]);
-				if ($cell->isValid())
-				{
-					$user->setDiscovered($cell->absX(), $cell->absY(), $radius);
-					$tpl->assign("msg", "Koordinaten erkundet!");
-				}
-			}
-			else
-			{
-				$tpl->assign("errmsg", "Ungültige Koordinate!");
-			}
-		}
-		// Reset discovered coordinates
-		else if (isset($_POST['discover_reset']))
-		{
-			$user->setDiscoveredAll(false);
-			$tpl->assign("msg", "Erkundung zurückgesetzt!");
-		}
-		// Discover all coordinates
-		else if (isset($_POST['discover_all']))
-		{
-			$user->setDiscoveredAll(true);
-			$tpl->assign("msg", "Alles erkundet!");
-		}
+        $res = dbQuerySave("
+            SELECT
+                id
+            FROM 
+                cells
+            WHERE 
+                sx=? 
+                AND sy=? 
+                AND cx=? 
+                AND cy=?;",
+            array(
+                $sx,
+                $sy,
+                $cx,
+                $cy
+            ));
+        if (mysql_num_rows($res)) {
+            $arr = mysql_fetch_row($res);
+            $cell = new Cell($arr[0]);
+            if ($cell->isValid()) {
+                $user->setDiscovered($cell->absX(), $cell->absY(), $radius);
+                $successMessage = 'Koordinaten erkundet!';
+            }
+        } else {
+            $errorMessage = 'Ungültige Koordinate!';
+        }
+    }
 
-		$tpl->assign("sx", $sx);
-		$tpl->assign("sy", $sy);
-		$tpl->assign("cx", $cx);
-		$tpl->assign("cy", $cy);
-		$tpl->assign("radius", $radius);
+    // Reset discovered coordinates
+    else if (isset($_POST['discover_reset'])) {
+        $user->setDiscoveredAll(false);
+        $successMessage = 'Erkundung zurückgesetzt!';
+    }
 
-		$tpl->assign("discoveredPercent", $user->getDiscoveredPercent());
-	}
-?>
+    // Discover all coordinates
+    else if (isset($_POST['discover_all'])) {
+        $user->setDiscoveredAll(true);
+        $successMessage = 'Alles erkundet!';
+    }
+
+    echo $twig->render('admin/galaxy/exploration.html.twig', [
+        'successMessage' => $successMessage,
+        'errorMessage' => $errorMessage,
+        'users' => $users,
+        'uid' => $uid,
+        'user' => $user,
+        'discoveredPercent' => $user->getDiscoveredPercent(),
+        'sx' => $sx,
+        'sy' => $sy,
+        'cx' => $cx,
+        'cy' => $cy,
+        'radius' => $radius,
+    ]);
+    exit();
+}
+
+echo $twig->render('admin/galaxy/exploration.html.twig', [
+    'successMessage' => $successMessage,
+    'errorMessage' => $errorMessage,
+    'users' => $users,
+    'uid' => $uid,
+]);
+exit();
