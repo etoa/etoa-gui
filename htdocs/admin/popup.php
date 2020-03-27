669b053cdd14aa67c6f56e269ab0f670bcbfc696
Stephan Vock <stephan.vock@gmail.com>
2020-03-27 21:04:25 +0000

    Admin: replace smarty with twig for popup

--- htdocs/admin/popup.php
+++ htdocs/admin/popup.php
@@ -12,43 +12,30 @@ ob_start();
 require("inc/includer.inc.php");
 
 try {
+    $twig->addGlobal('cssTheme', $css_theme);
+    $twig->addGlobal('pageTitle', getGameIdentifier() . ' Administration');
+    $twig->addGlobal('ajax_js', $xajax->getJavascript(XAJAX_DIR));
+    $twig->addGlobal('bodyTopStuff', getInitTT());
 
-// Create template object
-$tpl = new TemplateEngine('admin/tpl');
+    if ($s->user_id) {
+        $cu = new AdminUser($s->user_id);
 
-$tpl->setLayout("default/popup");
-$tpl->setView("default");
+        if (preg_match('/^[a-z\_]+$/',$page)  && strlen($page) <= 50) {
+            if (!include("content/".$page.".php"))
+                echo "<h1>Fehler</h1>Die Seite $page wurde nicht gefunden!";
+        } else {
+            echo "<h1>Fehler</h1>Der Seitenname <b>".$page."</b> enth&auml;lt unerlaubte Zeichen!<br><br><a href=\"javascript:history.back();\">Zur&uuml;ck</a>";
+        }
+    } else {
+        echo "<h1>Fehler</h1>Die Seite wurde nicht gefunden!";
+    }
 
-$tpl->assign("css_theme", $css_theme);
-$tpl->assign("page_title", getGameIdentifier()." Administration");
-$tpl->assign("ajax_js", $xajax->getJavascript(XAJAX_DIR));
-
-$tpl->assign("bodyTopStuff", getInitTT());
-
-if ($s->user_id)
-{
-	$cu = new AdminUser($s->user_id);
-	
-	if (preg_match('/^[a-z\_]+$/',$page)  && strlen($page)<=50)
-	{
-		if (!include("content/".$page.".php"))
-			echo "<h1>Fehler</h1>Die Seite $page wurde nicht gefunden!";
-	}
-	else {
-		echo "<h1>Fehler</h1>Der Seitenname <b>".$page."</b> enth&auml;lt unerlaubte Zeichen!<br><br><a href=\"javascript:history.back();\">Zur&uuml;ck</a>";
-	}	
-}
-else {
-	echo "<h1>Fehler</h1>Die Seite wurde nicht gefunden!";
-}
-
-$tpl->assign("content_overflow", ob_get_clean());
-$tpl->render();
+    echo $twig->render('admin/layout/popup.html.twig', [
+        'content' => ob_get_clean(),
+    ]);
 } catch (DBException $ex) {
-	ob_clean();
-	$tpl->setLayout("default/popup");
-	$tpl->assign("content_overflow", $ex);
-	$tpl->render();
+    require_once __DIR__ . '/../../src/minimalapp.php';
+    echo $app['twig']->render('layout/empty.html.twig', [
+        'content' => $ex,
+    ]);
 }
-	
-?>
