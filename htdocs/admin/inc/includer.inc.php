01fb050d1a5aa5a0e4730b9c744349ebc2300e90
Nicolas Perrenoud <mail@nicu.ch>
2021-06-14 11:50:22 +0200

    Remove whoops again, use already available symfony component from symfony/error-handler

--- htdocs/admin/inc/includer.inc.php
+++ htdocs/admin/inc/includer.inc.php
@@ -1,15 +1,17 @@
 <?PHP
 
+use Symfony\Component\ErrorHandler\Debug;
+use Symfony\Component\ErrorHandler\ErrorHandler;
+
 define('RELATIVE_ROOT', '../');
 define('ADMIN_MODE', true);
 require_once __DIR__ . '/../../../vendor/autoload.php';
 require_once __DIR__ . '/../../inc/bootstrap.inc.php';
 
 if (isDebugEnabled()) {
-    $whoops = new \Whoops\Run;
-    $whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler);
-    $whoops->register();
+    Debug::enable();
 }
+ErrorHandler::register();
 
 // Renderzeit-Start festlegen
 $render_time = explode(" ", microtime());
