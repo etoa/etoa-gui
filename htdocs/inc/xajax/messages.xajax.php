f2fdf4f50c39f6ae340ee22c839e203a5edf58fa
Nicolas Perrenoud <mail@nicu.ch>
2021-06-28 17:51:32 +0200

    Use message repo in xajax method

--- htdocs/inc/xajax/messages.xajax.php
+++ htdocs/inc/xajax/messages.xajax.php
@@ -1,45 +1,42 @@
 <?PHP
 
-$xajax->register(XAJAX_FUNCTION,'messagesSelectAllInCategory');
-$xajax->register(XAJAX_FUNCTION,'messagesSetRead');
+use EtoA\Message\MessageRepository;
+
+$xajax->register(XAJAX_FUNCTION, 'messagesSelectAllInCategory');
+$xajax->register(XAJAX_FUNCTION, 'messagesSetRead');
 
 //Selektiert alle Nachrichten in einer Kategorie
-function messagesSelectAllInCategory($cid,$cnt,$bv)
+function messagesSelectAllInCategory($cid, $cnt, $bv)
 {
     $objResponse = new xajaxResponse();
 
-    if ($bv=="-")
-    {
-	    for ($x=0;$x<$cnt;$x++)
-	    {
-		    $objResponse->assign("delcb_".$cid."_".$x, "checked","");
-		  }
-			$objResponse->assign("selectBtn[$cid]", "value","X");
+    if ($bv == "-") {
+        for ($x = 0; $x < $cnt; $x++) {
+            $objResponse->assign("delcb_" . $cid . "_" . $x, "checked", "");
+        }
+        $objResponse->assign("selectBtn[$cid]", "value", "X");
+    } else {
+        for ($x = 0; $x < $cnt; $x++) {
+            $objResponse->assign("delcb_" . $cid . "_" . $x, "checked", "true");
+        }
+        $objResponse->assign("selectBtn[$cid]", "value", "-");
     }
-    else
-    {
-	    for ($x=0;$x<$cnt;$x++)
-	    {
-		    $objResponse->assign("delcb_".$cid."_".$x, "checked","true");
-		  }
-			$objResponse->assign("selectBtn[$cid]", "value","-");
-		}
     return $objResponse;
 }
 
 function messagesSetRead($mid)
 {
-  $or = new xajaxResponse();
-  dbquery("UPDATE
-  	messages
-  SET
-  	message_read=1
-  WHERE
-  	message_id=".$mid."
-  LIMIT 1;");
-  $or->assign("msgimg".$mid,"src","images/pm_normal.gif");
-	return $or;
+    $or = new xajaxResponse();
+
+    // TODO
+    global $app;
+
+    /** @var MessageRepository */
+    $messageRepository = $app[MessageRepository::class];
+
+    $messageRepository->setRead($mid);
+
+    $or->assign("msgimg" . $mid, "src", "images/pm_normal.gif");
+
+    return $or;
 }
-
-
-?>
