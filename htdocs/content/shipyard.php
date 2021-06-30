0e5866e7cf48f95b70ca361bb72ebe781994fea3
Nicolas Perrenoud <mail@nicu.ch>
2021-06-30 18:24:36 +0200

    Migrated constant SHIP_BUILD_TIME

--- htdocs/content/shipyard.php
+++ htdocs/content/shipyard.php
@@ -583,7 +583,7 @@ $config = $app['etoa.config.service'];
 							+ $ships[$ship_id]['ship_costs_plastic']
 							+ $ships[$ship_id]['ship_costs_fuel']
 							+ $ships[$ship_id]['ship_costs_food'])
-							/ GLOBAL_TIME * SHIP_BUILD_TIME
+							/ GLOBAL_TIME * $config->getFloat('ship_build_time')
 							* $time_boni_factor
 							* $cu->specialist->shipTime;
 
@@ -994,7 +994,7 @@ $config = $app['etoa.config.service'];
 								}
 
 								// Bauzeit berechnen
-								$btime = ($data['ship_costs_metal'] + $data['ship_costs_crystal'] + $data['ship_costs_plastic'] + $data['ship_costs_fuel'] + $data['ship_costs_food']) / GLOBAL_TIME * SHIP_BUILD_TIME * $time_boni_factor * $cu->specialist->shipTime;
+								$btime = ($data['ship_costs_metal'] + $data['ship_costs_crystal'] + $data['ship_costs_plastic'] + $data['ship_costs_fuel'] + $data['ship_costs_food']) / GLOBAL_TIME * $config->getFloat('ship_build_time') * $time_boni_factor * $cu->specialist->shipTime;
 								$btime_min = $btime * (0.1 - ($gen_tech_level / 100));
 								$peopleOptimized= ceil(($btime-$btime_min)/$config->getInt('people_work_done'));
 
--- htdocs/inc/def.inc.php
+++ htdocs/inc/def.inc.php
@@ -43,9 +43,6 @@ define("GLOBAL_TIME", $config->getInt('global_time'));
 // Gebäudebau Faktor
 define("BUILD_BUILD_TIME", $config->getFloat('build_build_time'));
 
-// Schiffsbau Faktor
-define("SHIP_BUILD_TIME", $config->getFloat('ship_build_time'));
-
 // Verteidigungsbau Faktor
 define("DEF_BUILD_TIME", $config->getFloat('def_build_time'));
 
