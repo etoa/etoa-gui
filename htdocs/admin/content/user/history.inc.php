647ab7ed63dacd05f312699eaf744cc4b755247f
Stephan Vock <stephan.vock@gmail.com>
2021-07-18 21:34:50 +0100

    Functions: replace get_user_names with repository

--- htdocs/admin/content/user/history.inc.php
+++ htdocs/admin/content/user/history.inc.php
@@ -1,13 +1,18 @@
 <?PHP
+
+use EtoA\User\UserRepository;
+
+/** @var UserRepository $userRepository */
+$userRepository = $app[UserRepository::class];
 echo "<h1>User-History</h1>";
-$users = get_user_names();
+$userNicks = $userRepository->getUserNicknames();
 echo "<h2>Auswahl</h2>";
 echo "<form action=\"?page=$page&amp;sub=$sub\" method=\"post\">";
 echo "Spieler ausw&auml;hlen: <select name=\"user_id\">";
-foreach ($users as $id => $data) {
-    $hcnt = mysql_fetch_row(dbquery("SELECT COUNT(*) FROM user_history WHERE history_user_id='" . $id . "' ORDER BY history_timestamp ASC;"));
+foreach ($userNicks as $userId => $userNick) {
+    $hcnt = mysql_fetch_row(dbquery("SELECT COUNT(*) FROM user_history WHERE history_user_id='" . $userId . "' ORDER BY history_timestamp ASC;"));
     if ($hcnt[0] > 0)
-        echo "<option value=\"$id\">" . $data['nick'] . " (" . $hcnt[0] . " Eintr&auml;ge)</option>";
+        echo "<option value=\"$userId\">" . $userNick . " (" . $hcnt[0] . " Eintr&auml;ge)</option>";
 }
 echo "</select> <input type=\"submit\" name=\"submit\" value=\"Anzeigen\" />";
 
@@ -17,8 +22,10 @@ if ((isset($_POST['submit']) && $_POST['user_id'] > 0) || (isset($_GET['id']) &&
         $userid = $_POST['user_id'];
     else
         $userid = $_GET['id'];
-    echo "<h2>Geschichte der Users <a href=\"?page=user&sub=edit&user_id=" . $userid . "\">" . $users[$userid]['nick'] . "</a> (" . $users[$userid]['name'] . ", " . $users[$userid]['email'] . ")</h2>";
-    $hres = dbquery("SELECT * FROM user_history WHERE history_user_id='" . $userid . "' ORDER BY history_timestamp ASC;");
+
+    $user = $userRepository->getUser((int) $userid);
+    echo "<h2>Geschichte der Users <a href=\"?page=user&sub=edit&user_id=" . $user->id . "\">" . $user->nick . "</a> (" . $user->name . ", " . $user->email . ")</h2>";
+    $hres = dbquery("SELECT * FROM user_history WHERE history_user_id='" . $user->id . "' ORDER BY history_timestamp ASC;");
     if (mysql_num_rows($hres) > 0) {
         echo "<table>";
         echo "<tr><th class=\"tbltitle\" style=\"width:120px;\">Datum / Zeit</th><th class=\"tbltitle\">Ereignis</th></tr>";
