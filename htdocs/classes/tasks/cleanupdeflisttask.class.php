7439e582376ebb6464769e1cbda02cf3847a9d10
Nicolas Perrenoud <mail@nicu.ch>
2021-07-29 01:32:05 +0300

    Migrate ship/defense cleaup methods

--- htdocs/classes/tasks/cleanupdeflisttask.class.php
+++ htdocs/classes/tasks/cleanupdeflisttask.class.php
@@ -1,13 +1,25 @@
 <?PHP
 
+use EtoA\Defense\DefenseRepository;
+use Pimple\Container;
+
 /**
  * Remove old defense build list records
  */
 class CleanupDeflistTask implements IPeriodicTask
 {
+    private DefenseRepository $defenseRepository;
+
+    public function __construct(Container $app)
+    {
+        $this->defenseRepository = $app[DefenseRepository::class];
+    }
+
     function run()
     {
-        $nr = DefList::cleanUp();
+        $nr = $this->defenseRepository->cleanUp();
+        Log::add(Log::F_SYSTEM, Log::INFO, "$nr leere Verteidigungsdatensätze wurden gelöscht!");
+
         return "$nr alte Verteidigungseinträge gelöscht";
     }
 
