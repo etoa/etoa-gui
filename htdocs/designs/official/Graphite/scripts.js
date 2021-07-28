3eb2ec5293148805b1f1ec305c3289811c158d8f
Nicolas Perrenoud <mail@nicu.ch>
2021-07-28 18:59:30 +0300

    Code format js/css files in designs folder

--- htdocs/designs/official/Graphite/scripts.js
+++ htdocs/designs/official/Graphite/scripts.js
@@ -1,11 +1,10 @@
 window.design = "graphite";
 
-function hideAllHbarMenus()
-{
-    document.getElementById('hbarmapslide').style.display='none';
-    document.getElementById('hbarmessagesslide').style.display='none';
-    document.getElementById('hbarallianceslide').style.display='none';
-    document.getElementById('hbarsettingsslide').style.display='none';
-    document.getElementById('hbarhelpslide').style.display='none';
-    document.getElementById('hbarlogoutslide').style.display='none';
+function hideAllHbarMenus() {
+  document.getElementById('hbarmapslide').style.display = 'none';
+  document.getElementById('hbarmessagesslide').style.display = 'none';
+  document.getElementById('hbarallianceslide').style.display = 'none';
+  document.getElementById('hbarsettingsslide').style.display = 'none';
+  document.getElementById('hbarhelpslide').style.display = 'none';
+  document.getElementById('hbarlogoutslide').style.display = 'none';
 }
