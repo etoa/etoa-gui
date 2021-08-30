e988b034f8eabe249acf780085cde87acf62f913
Stephan Vock <stephan.vock@gmail.com>
2021-08-30 15:09:34 +0100

    Alliance: avoid division by 0 in test cases

--- src/Alliance/AllianceWithMemberCount.php
+++ src/Alliance/AllianceWithMemberCount.php
@@ -5,13 +5,15 @@ namespace EtoA\Alliance;
 class AllianceWithMemberCount extends Alliance
 {
     public int $memberCount;
-    public int $averagePoints;
+    public int $averagePoints = 0;
 
     public function __construct(array $data)
     {
         parent::__construct($data);
 
         $this->memberCount = (int) $data['member_count'];
-        $this->averagePoints = (int) floor($this->points / $this->memberCount);
+        if ($this->memberCount > 0) {
+            $this->averagePoints = (int) floor($this->points / $this->memberCount);
+        }
     }
 }
