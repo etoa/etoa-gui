9403d420a75f501b9ea7134ccae1deddcb82d051
Stephan Vock <stephan.vock@gmail.com>
2022-02-13 15:21:27 +0000

    PHPStan: fix dba config DB name

--- phpstan-dba-bootstrap.php
+++ phpstan-dba-bootstrap.php
@@ -16,12 +16,13 @@ $config = new RuntimeConfiguration();
 $config->debugMode(true);
 
 $databaseParts = parse_url($_SERVER['DATABASE_URL']);
+
 QueryReflection::setupReflector(
     new RecordingQueryReflector(
         ReflectionCache::create(
             $cacheFile
         ),
-        new \staabm\PHPStanDba\QueryReflection\PdoQueryReflector(new \PDO(sprintf('mysql:dbname=%s;host=%s', 'etoa', $databaseParts['host']), $databaseParts['user'], $databaseParts['pass']))
+        new \staabm\PHPStanDba\QueryReflection\PdoQueryReflector(new \PDO(sprintf('mysql:dbname=%s;host=%s', trim($databaseParts['path'], '/'), $databaseParts['host']), $databaseParts['user'], $databaseParts['pass']))
     ),
     $config
 );
