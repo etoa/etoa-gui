a17f2f15ff7c3fc8c25a7ea1a38303bf6471b148
Nicolas Perrenoud <mail@nicu.ch>
2023-06-23 23:26:20 +0300

    Added symfony 6 in subdirectory, with demo controller

---
+++ symfony/src/Controller/LuckyController.php
@@ -0,0 +1,20 @@
+<?php
+// src/Controller/LuckyController.php
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class LuckyController extends AbstractController
+{
+    #[Route('/lucky/number')]
+    public function number(): Response
+    {
+        $number = random_int(0, 100);
+
+        return $this->render('lucky/number.html.twig', [
+            'number' => $number,
+        ]);
+    }
+}
