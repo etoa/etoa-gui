77533d053a7538a77ad292125ca7e2170c65424d
Nicolas Perrenoud <nicolas.perrenoud@united-security-providers.ch>
2023-07-12 07:53:35 +0200

    Show controller cleanup

--- src/Controller/AbstractLegacyShowController.php
+++ src/Controller/AbstractLegacyShowController.php
@@ -3,7 +3,6 @@
 namespace EtoA\Controller;
 
 use EtoA\Design\Design;
-use EtoA\Legacy\User;
 use Symfony\Component\HttpFoundation\Response;
 
 abstract class AbstractLegacyShowController extends AbstractLegacyController
@@ -12,82 +11,44 @@ abstract class AbstractLegacyShowController extends AbstractLegacyController
 
     protected function handle(callable $callback): Response
     {
-        ob_start();
-
         $this->bootstrap();
 
-        // Set default page / action variables
-        $index = $_GET['index'] ?? null;
-        $mode = $_GET['mode'] ?? null;
-
-        $loggedIn = false;
-        $s = $this->userSession;
-        if ($s->validate(false)) {
-            $cu = new User($s->getUserId());
-            $loggedIn = (bool)$cu->isValid;
-        }
-
-        $properties = isset($cu) ? $this->userPropertiesRepository->getOrCreateProperties($cu->id) : null;
-
-        $design = Design::DIRECTORY . '/official/' . $this->config->get('default_css_style');
-        if (isset($cu) && filled($properties->cssStyle)) {
-            if (is_dir(Design::DIRECTORY . '/official/' . $properties->cssStyle)) {
-                $design = Design::DIRECTORY . '/official/' . $properties->cssStyle;
-            }
-        }
-        define('CSS_STYLE', $design);
-
-//        $xajax = require_once $projectDir . '/src/xajax/xajax.inc.php';
-//        $twig->addGlobal('xajaxJS', $xajax->getJavascript());
+//        $xajax = require_once $this->projectDir . '/src/xajax/xajax.inc.php';
+//        $this->twig->addGlobal('xajaxJS', $xajax->getJavascript());
         $globals = [
             'gameTitle' => $this->versionService->getGameIdentifier() . ($this->pageTitle !== null ? ' - ' . $this->pageTitle : ''),
-            'templateDir' => '/' . CSS_STYLE,
+            'templateDir' => '/' . Design::DIRECTORY . '/official/' . $this->config->get('default_css_style'),
             'bodyTopStuff' => getInitTT(),
-            'prevPlanetId' => null,
-            'nextPlanetId' => null,
             'enableKeybinds' => false,
-            'mode' => $mode,
             'viewportScale' => $_SESSION['viewportScale'] ?? 0,
             'fontSize' => ($_SESSION['viewportScale'] ?? 1) * 16 . "px",
         ];
-
         foreach ($globals as $key => $value) {
             $this->twig->addGlobal($key, $value);
         }
 
-        //
-        // Page content
-        //
-
-        $show = true;
-        $invalidKey = false;
         // Handle case if outgame key is set
         if ($this->config->filled('register_key')) {
+            $invalidKey = false;
             if (isset($_POST['reg_key_auth_submit'])) {
                 if ($_POST['reg_key_auth_value'] === $this->config->get('register_key')) {
-                    $s->reg_key_auth = $this->config->get('register_key');
+                    $this->userSession->reg_key_auth = $this->config->get('register_key');
                 } else {
                     $invalidKey = true;
                 }
             }
 
-            if ($s->reg_key_auth !== $this->config->get('register_key')) {
-                $show = false;
+            if ($this->userSession->reg_key_auth !== $this->config->get('register_key')) {
+                return $this->render('external/key-required.html.twig', [
+                    'page' => $_GET['index'],
+                    'invalidKey' => $invalidKey,
+                ]);
             }
         }
 
-        if ($loggedIn) {
-            $show = true;
-        }
+        ob_start();
 
-        if ($show) {
-            $callback();
-        } else {
-            echo $this->twig->render('external/key-required.html.twig', [
-                'page' => $_GET['index'],
-                'invalidKey' => $invalidKey,
-            ]);
-        }
+        $callback();
 
         return new Response(ob_get_clean());
     }
