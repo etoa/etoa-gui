7c5c6fb456610287013ea2dc15e25e42b382f208
Nicolas Perrenoud <mail@nicu.ch>
2023-07-14 15:33:59 +0200

    Remove legacy error code handling

--- src/Controller/External/LoginController.php
+++ src/Controller/External/LoginController.php
@@ -32,11 +32,6 @@ class LoginController extends AbstractLegacyShowController
                 return $this->redirect('/'); // TODO
             }
 
-            if ($request->query->has('err')) {
-                $errCode = $request->query->get('err');
-                $this->addFlash('error', $this->getErrMsg($errCode));
-            }
-
             // TODO CSRF token
 
             return $this->render('external/login.html.twig', [
@@ -44,26 +39,5 @@ class LoginController extends AbstractLegacyShowController
                 'roundName' => $this->config->get('roundname'),
             ]);
         });
-    }
-
-    private function getErrMsg(string $err): string
-    {
-        return match ($err) {
-            "name" => "Du hast vergessen einen Namen oder ein Passwort einzugeben!",
-            "pass" => "Falsches Passwort oder falscher Benutzername!",
-            "ip" => "IP-Adresse-Überprüfungsfehler! Kein Login von diesem Computer möglich, da schon eine andere IP mit diesem Account verbunden ist!",
-            "timeout" => "Das Timeout wurde erreicht und du wurdest automatisch ausgeloggt!",
-            "session" => "Session-Cookie-Fehler. Überprüfe ob dein Browser wirklich Sitzungscookies akzeptiert!",
-            "tomanywindows" => "Es wurden zu viele Fenster geöffnet oder aktualisiert, dies ist leider nicht erlaubt!",
-            "session2", "nosession" => "Deine Session ist nicht mehr vorhanden! Sie wurde entweder gelöscht oder sie ist fehlerhaft. Dies kann passieren wenn du dich an einem anderen PC einloggst obwohl du noch mit diesem online warst!",
-            "verification" => "Falscher Grafikcode! Bitte gib den linksstehenden Code in der Grafik korrekt in das Feld darunter ein!
-            Diese Massnahme ist leider nötig um das Benutzen von automatisierten Programmen (Bots) zu erschweren.",
-            "logintimeout" => "Der Login-Schlüssel ist abgelaufen! Bitte logge dich neu ein!",
-            "sameloginkey" => "Der Login-Schlüssel wurde bereits verwendet! Bitte logge dich neu ein!",
-            "wrongloginkey" => "Falscher Login-Schlüssel! Ein Login ist nur von der offiziellen EtoA-Startseite aus möglich!",
-            "nologinkey" => "Kein Login-Schlüssel! Ein Login ist nur von der offiziellen EtoA-Startseite aus möglich!",
-            "general" => "Ein allgemeiner Fehler ist aufgetreten. Bitte den Entwickler kontaktieren!",
-            default => "Unbekannter Fehler (" . $err . "). Bitte den Entwickler kontaktieren!",
-        };
     }
 }
