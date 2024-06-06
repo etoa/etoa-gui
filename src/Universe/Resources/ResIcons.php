aef4d51b96e2c3089308405b9d346c49ff6e1c4e
coo1ness <coo1ness@etoa.ch>
2024-06-06 17:10:58 +0200

    fixed icon path

--- src/Universe/Resources/ResIcons.php
+++ src/Universe/Resources/ResIcons.php
@@ -4,16 +4,16 @@ namespace EtoA\Universe\Resources;
 
 class ResIcons
 {
-    public const METAL = '<img class="resIcon" src="images/resources/metal_s.png" alt="' . ResourceNames::METAL . '" />';
-    public const CRYSTAL = '<img class="resIcon" src="images/resources/metal_s.png" alt="' . ResourceNames::CRYSTAL . '" />';
-    public const PLASTIC = '<img class="resIcon" src="images/resources/metal_s.png" alt="' . ResourceNames::PLASTIC . '" />';
-    public const FUEL = '<img class="resIcon" src="images/resources/metal_s.png" alt="' . ResourceNames::FUEL . '" />';
-    public const FOOD = '<img class="resIcon" src="images/resources/metal_s.png" alt="' . ResourceNames::FOOD . '" />';
-    public const POWER = '<img class="resIcon" src="images/resources/metal_s.png" alt="' . ResourceNames::POWER . '" />';
-    public const POWER_USE = '<img class="resIcon" src="images/resources/metal_s.png" alt="Energieverbrauch" />';
-    public const PEOPLE = '<img class="resIcon" src="images/resources/metal_s.png" alt="Bevölkerung" />';
-    public const TIME = '<img class="resIcon" src="images/resources/metal_s.png" alt="' . ResourceNames::TIME . '" />';
-    public const FIELDS = '<img class="resIcon" src="images/resources/metal_s.png" alt="' . ResourceNames::FIELDS . '" />';
+    public const METAL = '<img class="resIcon" src="/build/images/resources/metal_s.png" alt="' . ResourceNames::METAL . '" />';
+    public const CRYSTAL = '<img class="resIcon" src="/build/images/resources/crystal_s.png" alt="' . ResourceNames::CRYSTAL . '" />';
+    public const PLASTIC = '<img class="resIcon" src="/build/images/resources/plastic_s.png" alt="' . ResourceNames::PLASTIC . '" />';
+    public const FUEL = '<img class="resIcon" src="/build/images/resources/fuel_s.png" alt="' . ResourceNames::FUEL . '" />';
+    public const FOOD = '<img class="resIcon" src="/build/images/resources/food_s.png" alt="' . ResourceNames::FOOD . '" />';
+    public const POWER = '<img class="resIcon" src="/build/images/resources/power_s.png" alt="' . ResourceNames::POWER . '" />';
+    public const POWER_USE = '<img class="resIcon" src="/build/images/resources/poweru_s.png" alt="Energieverbrauch" />';
+    public const PEOPLE = '<img class="resIcon" src="/build/images/resources/people_s.png" alt="Bevölkerung" />';
+    public const TIME = '<img class="resIcon" src="/build/images/resources/time_s.png" alt="' . ResourceNames::TIME . '" />';
+    public const FIELDS = '<img class="resIcon" src="/build/images/resources/field_s.png" alt="' . ResourceNames::FIELDS . '" />';
 
     public const ICONS = [
         self::METAL,
