e5c990c19c0afbe73c2fcc89b8c14f416d2d963d
coo1ness <coo1ness@etoa.ch>
2024-06-21 17:02:58 +0200

    cleanup race setup

--- src/Form/Type/Core/RaceSetupType.php
+++ src/Form/Type/Core/RaceSetupType.php
@@ -2,16 +2,9 @@
 
 namespace EtoA\Form\Type\Core;
 
-use EtoA\Form\Type\Core\EntityType;
-use EtoA\Form\Type\Core\PositiveIntegerType;
-use EtoA\Form\Type\Core\ShipType;
-use EtoA\Universe\Entity\EntityLabelSearch;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
-use EtoA\Race\RaceDataRepository;
-use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\ButtonType;
 use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 
@@ -20,12 +13,12 @@ class RaceSetupType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('shipId', RaceType::class, [
+            ->add('raceId', RaceType::class, [
                 'placeholder' => 'Bitte wählen...',
                 'label' => false,
                 'required' => true,
             ])
-            ->add('save', SubmitType::class, [
+            ->add('saveRace', SubmitType::class, [
                 'label' => 'Weiter',
             ])
             ->add('random', ButtonType::class, [
