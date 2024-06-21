a24da6f421f45838af1ff8a007e0102cdf28cdb4
coo1ness <coo1ness@etoa.ch>
2024-06-21 16:56:50 +0200

    added design service

---
+++ src/Design/DesignService.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace EtoA\Design;
+
+use EtoA\Core\Configuration\ConfigurationService;
+use EtoA\User\UserPropertiesRepository;
+use Symfony\Bundle\SecurityBundle\Security;
+
+class DesignService
+{
+    public function __construct(
+        private readonly UserPropertiesRepository $userPropertiesRepository,
+        private readonly ConfigurationService     $config,
+        private readonly Security $security,
+    )
+    {
+    }
+
+    public function getCurrentDesign():string
+    {
+        $cu = $this->security->getUser();
+        $properties = $this->userPropertiesRepository->getOrCreateProperties($cu->getId());
+
+        $design = Design::DIRECTORY . "/official/" . $this->config->get('default_css_style');
+        if (filled($properties->cssStyle)) {
+            if (is_dir(Design::DIRECTORY . "/official/" . $properties->cssStyle)) {
+                $design = Design::DIRECTORY . "/official/" . $properties->cssStyle;
+            }
+        }
+
+        return $design;
+    }
+}
