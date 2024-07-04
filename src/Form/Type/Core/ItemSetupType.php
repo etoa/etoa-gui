0b1fe0fc0b3dec89f27b2a9af382219c1516f5e4
coo1ness <coo1ness@etoa.ch>
2024-07-04 21:07:07 +0200

    cleanup

--- src/Form/Type/Core/ItemSetupType.php
+++ src/Form/Type/Core/ItemSetupType.php
@@ -5,16 +5,12 @@ namespace EtoA\Form\Type\Core;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 
 class ItemSetupType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('checker', HiddenType::class, [
-                #'data' => $options['data'][0],
-            ])
             ->add('itemset_id', DefaultItemType::class, [
                 'placeholder' => 'Bitte wählen...',
                 'label' => false,
