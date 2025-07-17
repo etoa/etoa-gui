5954021d6daa3aac02337313fb55d06af7297ab1
coo1ness <coo1ness@etoa.ch>
2025-07-17 21:57:52 +0200

    added RTE

--- config/bundles.php
+++ config/bundles.php
@@ -16,4 +16,5 @@ return [
     Scheb\TwoFactorBundle\SchebTwoFactorBundle::class => ['all' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     Symfony\UX\StimulusBundle\StimulusBundle::class => ['all' => true],
+    FOS\CKEditorBundle\FOSCKEditorBundle::class => ['all' => true],
 ];
