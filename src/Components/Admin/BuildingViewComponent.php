e2cc74ef2480fc0efcba429b70bb72c2565e36d8
coo1ness <coo1ness@etoa.ch>
2025-08-20 20:50:54 +0200

    fixed admin buildlist

--- src/Components/Admin/BuildingViewComponent.php
+++ src/Components/Admin/BuildingViewComponent.php
@@ -8,7 +8,6 @@ use EtoA\Components\Helper\AbstractEditComponent;
 use EtoA\Entity\BuildingListItem;
 use EtoA\Form\Type\Admin\EditBuildingType;
 use Symfony\Component\Form\FormInterface;
-use Symfony\Component\Form\FormView;
 use Symfony\UX\LiveComponent\Attribute\AsLiveComponent;
 use Symfony\UX\LiveComponent\Attribute\LiveAction;
 use Symfony\UX\LiveComponent\Attribute\LiveProp;
@@ -16,28 +15,14 @@ use Symfony\UX\LiveComponent\Attribute\LiveProp;
 #[AsLiveComponent('admin_building_view')]
 class BuildingViewComponent extends AbstractEditComponent
 {
-    #[LiveProp]
-    public int $itemId;
-    #[LiveProp]
-    public string $user;
-    #[LiveProp]
-    public string $building;
-    #[LiveProp]
-    public string $entity;
-    private ?BuildingListItem $item = null;
+    #[LiveProp(writable: true)]
+    public ?BuildingListItem $item = null;
+
     /** @var array<int, string> */
     public array $buildTypes;
 
-    public function mount(FormView $view = null, BuildingListItem $item = null): void
-    {
-        $this->item = $item;
-        if ($item !== null) {
-            $this->itemId = $item->id;
-        }
-    }
-
     public function __construct(
-        private BuildingListItemRepository $buildingRepository,
+        private readonly BuildingListItemRepository $buildingRepository,
     ) {
         $this->buildTypes = BuildingBuildType::all();
     }
@@ -45,16 +30,12 @@ class BuildingViewComponent extends AbstractEditComponent
     #[LiveAction]
     public function delete(): void
     {
-        $this->buildingRepository->removeEntry($this->itemId);
+        $this->buildingRepository->remove($this->item);
         $this->item = null;
     }
 
     public function getItem(): ?BuildingListItem
     {
-        if ($this->item === null) {
-            $this->item = $this->buildingRepository->getEntry($this->itemId);
-        }
-
         return $this->item;
     }
 
@@ -65,6 +46,6 @@ class BuildingViewComponent extends AbstractEditComponent
 
     protected function storeItem(): void
     {
-        $this->buildingRepository->save($this->item);
+        $this->buildingRepository->save();
     }
 }
