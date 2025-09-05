56d6d2db4541bd6cab33bfe5aaf1d1bda7255463
coo1ness <coo1ness@etoa.ch>
2025-09-05 14:55:41 +0200

    fixed admin alliance diplomacy

---
+++ src/Form/Type/Admin/AllianceDiplomacyType.php
@@ -0,0 +1,44 @@
+<?php declare(strict_types=1);
+
+namespace EtoA\Form\Type\Admin;
+
+use EtoA\Alliance\AllianceDiplomacyLevel;
+use EtoA\Entity\AllianceDiplomacy;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class AllianceDiplomacyType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder->addEventListener(FormEvents::PRE_SET_DATA, function (FormEvent $event) use ($builder) {
+            $event
+                ->getForm()
+                ->add('name', TextType::class, [
+                    'attr' => [
+                        'size' => 35
+                    ]
+                ])
+                ->add('level', ChoiceType::class, [
+                    'choices' => array_flip(AllianceDiplomacyLevel::all()),
+                ])
+                ->add('delete', CheckboxType::class, [
+                    'required' => false,
+                    'mapped' => false
+                ]);
+        });
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => AllianceDiplomacy::class,
+        ]);
+    }
+}
