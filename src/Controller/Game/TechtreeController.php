f76e4589b2b3c751e202b7235e2d81c7ec128877
coo1ness <coo1ness@etoa.ch>
2026-06-23 19:19:11 +0200

    techtree wip

--- src/Controller/Game/TechtreeController.php
+++ src/Controller/Game/TechtreeController.php
@@ -2,14 +2,64 @@
 
 namespace EtoA\Controller\Game;
 
-use EtoA\Controller\Game\AbstractGameController;
+use EtoA\Entity\Planet;
+use EtoA\Universe\Planet\PlanetRepository;
+use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class TechtreeController extends AbstractGameController
 {
-    #[Route('/game/techtree', name: 'game.techtree')]
-    public function overview(): Response {
-        return $this->render('game/techtree/techtree.html.twig');
+
+
+    public function __construct(
+        private readonly PlanetRepository $planetRepository,
+        private readonly RequestStack $requestStack
+    )
+    {
+    }
+
+    #[Route('/game/techtree/general', name: 'game.techtree.general')]
+    public function general(): Response
+    {
+        return $this->render('game/techtree/general.html.twig', [
+            'planet' => $this->getCurrentPlanet()
+        ]);
+    }
+
+    #[Route('/game/techtree/building', name: 'game.techtree.building')]
+    public function building(): Response
+    {
+        return $this->render('game/techtree/building.html.twig');
+    }
+
+    #[Route('/game/techtree/tech', name: 'game.techtree.tech')]
+    public function tech(): Response
+    {
+        return $this->render('game/techtree/tech.html.twig');
+    }
+
+    #[Route('/game/techtree/ships', name: 'game.techtree.ships')]
+    public function ships(): Response
+    {
+        return $this->render('game/techtree/ships.html.twig');
+    }
+
+    #[Route('/game/techtree/defense', name: 'game.techtree.defense')]
+    public function defense(): Response
+    {
+        return $this->render('game/techtree/defense.html.twig');
+    }
+
+    #[Route('/game/techtree/missile', name: 'game.techtree.missile')]
+    public function missile(): Response
+    {
+        return $this->render('game/techtree/missile.html.twig');
+    }
+
+    private function getCurrentPlanet(): Planet
+    {
+        $request = $this->requestStack->getCurrentRequest();
+        return $this->planetRepository->find($request->getSession()->get('cpid'));
     }
 }
