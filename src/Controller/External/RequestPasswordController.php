3f3a92056189903f0bde9efaca75649af0b435d1
coo1ness <coo1ness@etoa.ch>
2026-08-18 16:03:55 +0200

    refactored password forget

--- src/Controller/External/RequestPasswordController.php
+++ src/Controller/External/RequestPasswordController.php
@@ -2,49 +2,86 @@
 
 namespace EtoA\Controller\External;
 
-use EtoA\Controller\AbstractLegacyShowController;
+use EtoA\Core\Configuration\ConfigurationService;
 use EtoA\Support\Checker;
 use EtoA\User\UserService;
 use Exception;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use EtoA\Support\ValidationUtils;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
-class RequestPasswordController extends AbstractLegacyShowController
+class RequestPasswordController extends AbstractController
 {
-    protected ?string $pageTitle = 'Neues Passwort';
+
+
+    public function __construct(
+        private readonly ConfigurationService $configurationService,
+        private readonly UserService $userService,
+    )
+    {
+    }
 
     #[Route('/request-password', name: 'external.request-password')]
     public function index(
-        UserService $userService,
         Request     $request,
-        Checker     $checker,
     ): Response
     {
-        return $this->handle(function () use (
-            $userService,
-            $request,
-            $checker,
-        ) {
-            if ($request->request->has('submit_pwforgot') && $checker->checker_verify()) {
-                if (ValidationUtils::filled($request->request->get('user_nick')) && ValidationUtils::filled($request->request->get('user_email_fix'))) {
-                    try {
-                        $userService->resetPassword($request->request->get('user_nick'), $request->request->get('user_email_fix'));
-                        $this->addFlash('success', 'Deine Passwort-Anfrage war erfolgreich. Du solltest in einigen Minuten eine E-Mail mit dem neuen Passwort erhalten!');
-                        return $this->redirectToRoute('external.login');
-                    } catch (Exception $ex) {
-                        $this->addFlash('error', $ex->getMessage());
-                    }
-                } else {
-                    $this->addFlash('error', 'Du hast keinen Benutzernamen oder keine E-Mail-Adresse eingegeben oder ein unerlaubtes Zeichen verwendet!');
+        $form = $this->createFormBuilder(options: ['attr'=>['class'=>'styled-form styled-form-medium']])
+            ->add('nick', TextType::class, [
+                'label' => false,
+                'attr' => [
+                    'maxlength' => $this->configurationService->param2('nick_length'),
+                    'size' => 30,
+                    'autofocus' => 1
+                ],
+                'constraints' => [
+                    new NotBlank(),
+                ],
+            ])
+            ->add('email', EmailType::class, [
+                'label' => false,
+                'attr' => [
+                    'maxlength' => 50,
+                    'size' => 30,
+                ],
+                'constraints' => [
+                    new NotBlank(),
+                ],
+            ])
+            ->add('submit', SubmitType::class, [
+                'label' => 'Passwort anfordern',
+                'attr' => [
+                    'class' => 'button'
+                ]
+            ])
+            ->getForm()
+            ->handleRequest($request);
+
+
+        if ($form->isSubmitted()) {
+            if($form->isValid()) {
+                try {
+                    $this->userService->resetPassword($form->get('nick')->getData(), $form->get('email')->getData());
+                    $this->addFlash('success', 'Deine Passwort-Anfrage war erfolgreich. Du solltest in einigen Minuten eine E-Mail mit dem neuen Passwort erhalten!');
+                    return $this->redirectToRoute('external.login');
+                } catch (Exception $ex) {
+                    $this->addFlash('error', $ex->getMessage());
                 }
             }
+            else {
+                $this->addFlash('error', 'Du hast keinen Benutzernamen oder keine E-Mail-Adresse eingegeben oder ein unerlaubtes Zeichen verwendet!');
+            }
+        }
 
-            return $this->render('external/pwforgot.html.twig', [
-                'roundName' => $this->config->get('roundname'),
-                'checker' => $checker->checker_init(),
-            ]);
-        });
+        return $this->render('external/pwforgot.html.twig', [
+            'roundName' => $this->configurationService->get('roundname'),
+            'form' => $form
+        ]);
     }
 }
